de33beffb7b30132255b7994317719617cd3bb0d
MANUEL RIOS <m.rios56@hotmail.com>
2023-10-07 19:30:12 +0000

    adding welcome text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 
-import MdxComponent from "@/components/MdxComponent";
 import Hero from '@/components/Hero'
-import { getFileContent } from '@/helpers'
 import Cards from '@/components/ui/Cards'
 import { cardsConfig } from "@/config";
 
 export default async function Home() {
-  const markdown = await getFileContent('site/usingzcash/usingzec.md');
-  const content = markdown ? markdown : '# No Data'
 
+const text = `ZecHub is the community driven education hub for the Zcash cryptocurrency (ZEC). Zcash is a digital currency providing censorship resistant, secure & private payments. The Zcash Blockchain utilises highly advanced 'verifiable' zk-snarks that do not require Trusted Setup following the NU5 network upgrade in 2022.`
   return (
     <main className="flex flex-col mx-auto ">
       <Hero />
@@ -18,9 +15,9 @@ export default async function Home() {
           <div className="flex flex-col items-center justify-center p-3 mt-6 shadow">
             <h1 className="text-4xl font-bold mb-3">Welcome to ZecHub</h1>
             <div className="flex items-center justify-center p-4 ">
-            <p className="text-lg font-bold text-center">
-                  ZecHub is the community driven education hub for the Zcash cryptocurrency (ZEC). Zcash is a digital currency providing censorship resistant, secure & private payments. The Zcash Blockchain utilises highly advanced 'verifiable' zk-snarks that do not require Trusted Setup following the NU5 network upgrade in 2022.
-                </p>
+              <p className="text-lg font-bold text-center">
+                {text}
+              </p>
             </div>
           </div>
 
