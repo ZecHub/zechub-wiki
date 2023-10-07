07df5a8c7222a0d9f00c29c224b51cf6dfb891d6
MANUEL RIOS <m.rios56@hotmail.com>
2023-10-07 19:25:34 +0000

    adding welcome text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,15 +15,17 @@ export default async function Home() {
       <div className="flex flex-col">
         <div className="w-full flex  items-center justify-center">
 
-          <div className="flex flex-col items-center justify-center p-3 mt-6">
-            <h1 className="text-4xl font-bold mb-3">Using Zcash</h1>
-            <div className="flex items-center justify-center p-4 shadow">
-              <MdxComponent source={content} />
+          <div className="flex flex-col items-center justify-center p-3 mt-6 shadow">
+            <h1 className="text-4xl font-bold mb-3">Welcome to ZecHub</h1>
+            <div className="flex items-center justify-center p-4 ">
+            <p className="text-lg font-bold text-center">
+                  ZecHub is the community driven education hub for the Zcash cryptocurrency (ZEC). Zcash is a digital currency providing censorship resistant, secure & private payments. The Zcash Blockchain utilises highly advanced 'verifiable' zk-snarks that do not require Trusted Setup following the NU5 network upgrade in 2022.
+                </p>
             </div>
           </div>
 
         </div>
-        <div className="p-5 flex flex-col md:flex-row md:space-x-11 items-center justify-center">
+        <div className="p-5 flex flex-col space-y-7 md:flex-row md:space-x-11 items-center justify-center">
           {
             cardsConfig.map((items) => (
               <Cards key={items.title} paraph={items.content} title={items.title} url={items.url} />
