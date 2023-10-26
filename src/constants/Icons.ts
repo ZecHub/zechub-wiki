e5075293cb15a8f48c610cd03371e1b8cdfa9855
MANUEL RIOS <m.rios56@hotmail.com>
2023-10-26 21:32:32 +0000

    reorder the cards

---
+++ src/constants/Icons.ts
@@ -0,0 +1,3 @@
+import {icons}  from 'react-icons'
+
+
--- src/constants/exploreFolders.ts
+++ src/constants/exploreFolders.ts
@@ -2,51 +2,33 @@ export const exploreFolders = [
     {
         img: '/getZec.png',
         description: '',
-        name: `Glossary_and_FAQs`,
-        url: '/site/Glossary_and_FAQs/FAQ',
-    },
-    {
-        img: '/getZec.png',
-        description: '',
-        name: 'Privacy_Tools',
-        url: '/site/Privacy_Tools/2FA_Hardware_Devices',
-    },
-
-    {
-        img: '/getZec.png',
-        description: '',
-        name: 'Research',
-        url: '/site/Research/Social_Media_Data_Collection',
-    },
-
-    {
-        img: '/getZec.png',
-        description: '',
         name: 'Start_Here',
         url: '/site/Start_Here/Developer_Resources',
     },
-
+    {
+        img: '/getZec.png',
+        description: '',
+        name: 'Tutorials',
+        url: '/site/tutorials/Buy_ZEC_in_Gemini',
+    },
     {
         img: '/getZec.png',
         description: '',
         name: 'Using_Zcash',
         url: '/site/Using_Zcash/Buying_ZEC',
     },
-
     {
         img: '/getZec.png',
         description: '',
-        name: 'ZFAV_Club',
-        url: '/site/ZFAV_Club/AV_Club_Background',
+        name: 'Guides',
+        url: '/site/guides/Free2z_Live',
     },
-
     {
         img: '/getZec.png',
         description: '',
-        name: 'Zcash_Community',
-        url: '/site/Zcash_Community/Arborist_Calls',
+        name: 'Zcash_Tech',
+        url: '/site/Zcash_Tech/Blockchain_Explorers',
     },
-
     {
         img: '/getZec.png',
         description: '',
@@ -56,39 +38,43 @@ export const exploreFolders = [
     {
         img: '/getZec.png',
         description: '',
-        name: 'Zcash_Social_Media',
-        url: '/site/Zcash_Social_Media/Podcasts',
+        name: 'Zcash_Community',
+        url: '/site/Zcash_Community/Arborist_Calls',
     },
-
     {
         img: '/getZec.png',
         description: '',
-        name: 'Zcash_Tech',
-        url: '/site/Zcash_Tech/Blockchain_Explorers',
+        name: 'ZFAV_Club',
+        url: '/site/ZFAV_Club/AV_Club_Background',
     },
-
+    {
+        img: '/getZec.png',
+        description: '',
+        name: 'Zcash_Social_Media',
+        url: '/site/Zcash_Social_Media/Podcasts',
+    },
+    {
+        img: '/getZec.png',
+        description: '',
+        name: 'Privacy_Tools',
+        url: '/site/Privacy_Tools/2FA_Hardware_Devices',
+    },
+    {
+        img: '/getZec.png',
+        description: '',
+        name: 'Research',
+        url: '/site/Research/Social_Media_Data_Collection',
+    },
+    {
+        img: '/getZec.png',
+        description: '',
+        name: `Glossary_and_FAQs`,
+        url: '/site/Glossary_and_FAQs/FAQ',
+    },
     {
         img: '/getZec.png',
         description: '',
         name: 'Contribute',
         url: '/site/contribute/Help_build_ZecHub',
     },
-
-    {
-        img: '/getZec.png',
-        description: '',
-        name: 'Guides',
-        url: '/site/guides/Free2z_Live',
-    },
-
-    {
-        img: '/getZec.png',
-        description: '',
-        name: 'Tutorials',
-        url: '/site/tutorials/Buy_ZEC_in_Gemini',
-    },
-
-
-
-
 ]
