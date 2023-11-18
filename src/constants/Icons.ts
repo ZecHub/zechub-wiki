e57f70421169584b867b6228a3e23e4f42616ed5
squirrel <47972284+JJ-Rogers@users.noreply.github.com>
2023-11-18 15:09:17 +0000

    Update Icons.ts

--- src/constants/Icons.ts
+++ src/constants/Icons.ts
@@ -1,10 +1,10 @@
 import {SiZcash as Zcash, SiWikipedia as Wikipedia, SiYoutube as Youtube, SiXdadevelopers as Dadeveloper, SiRaspberrypi as RaspBerry, SiApplepodcasts as ApplePodcasts, SiTorbrowser as TorBrowser }  from 'react-icons/si'
-import {PiShootingStarThin as ShootingStar, PiCircuitryLight as Circuitry, PiNumberCircleZeroThin as NumberCircle, PiSnowflakeThin as SnowFlake, PiLinkSimpleBold as LinkSimpleBold, PiPresentationChartLight as PresentationChar, PiSpotifyLogo as Spotify } from 'react-icons/pi'
+import {PiShootingStarThin as ShootingStar, PiCircuitryLight as Circuitry, PiNumberCircleZeroThin as NumberCircle, PiSnowflakeThin as SnowFlake, PiLinkSimpleBold as LinkSimpleBold, PiPresentationChartLight as PresentationChar, PiSpotifyLogo as Spotify, PiComputerTower as ComputerTower } from 'react-icons/pi'
 import {RiSecurePaymentLine as SecurePayment, RiMailSendFill as MailSend, RiExchangeFundsFill as ExchangeFunds, RiLiveFill as Live, RiNftFill as NftFill, RiMessengerLine as MessengerLine, RiArticleLine } from 'react-icons/ri'
 import {GrResources as Resources, GrCircleInformation as CircleInfo } from 'react-icons/gr'
 import {TbBinaryTree2 as BinaryTree, TbTopologyRing as TopologyRing, TbCurrencyDollarZimbabwean as DollarZimbabwean,  Tb2Fa as Fa2, TbHexagonLetterN as HexagonLetterN, TbHexagonLetterZ as HexagonLetterZ} from 'react-icons/tb'
 import {SlGraph as Graph } from 'react-icons/sl'
-import {FaWallet as Wallet, FaFaucet as Faucet, FaListAlt as ListAlt, FaEthereum as Ethereum, FaQuestion } from 'react-icons/fa'
+import {FaWallet as Wallet, FaFaucet as Faucet, FaListAlt as ListAlt, FaEthereum as Ethereum, FaQuestion, FaSyncAlt as Syncing} from 'react-icons/fa'
 import {FcCurrencyExchange as CurrencyExchange, FcGallery, FcGlobe as Globe } from 'react-icons/fc'
 import {BsFillArrowUpRightCircleFill as ArrowUp, BsShieldShaded as ShieldShaded, BsQrCode as QrCode, BsPlay as Play, BsNewspaper as Newspaper } from 'react-icons/bs'
 import {MdOutlinePrivacyTip as PrivacyTip, MdOutlinePointOfSale as PointOfSale, MdRadar as Radar, MdTipsAndUpdates as TipsAndUpdates, MdSettings as Settings, MdOutlineEnhancedEncryption as OutlineEnchanceEncryption } from 'react-icons/md'
@@ -17,6 +17,8 @@ import {IoBuildOutline as BuildOutline, IoConstructOutline } from 'react-icons/i
 import {IoIosMicrophone as IosMicrophone} from 'react-icons/io'
 import {CiStreamOn as StreamOn} from 'react-icons/ci' 
 import {HiOutlineBuildingLibrary} from 'react-icons/hi2'
+import { LiaHorseHeadSolid } from "react-icons/lia";
+
 
 interface IconsFor {
     [key: string]: {
@@ -53,6 +55,7 @@ const iconsForMenu: IconsFor = {
         'Faucets': Faucet,
         'Non-Custodial Exchanges': ExchangeFunds,
         'DEX List': ListAlt,
+        'Zcash Wallet Syncing': Syncing,
     },
     'Guides': {
         'Using ZEC Privately': PrivacyTip,
@@ -72,6 +75,7 @@ const iconsForMenu: IconsFor = {
         'FROST': SnowFlake,
         'Viewing Keys': FileKey,
         'Zcash Shielded Assets': DollarZimbabwean,
+        'Lightwallet Nodes': ComputerTower.
     },
     'Zcash Organizations': {
         'Electric Coin Company': Radar,
@@ -88,6 +92,11 @@ const iconsForMenu: IconsFor = {
         'Zcash Ecosystem Security': Lock,
         'Zcash Media': Play,
         'Zcon Archive': PresentationChar,
+    },
+        'ZFAV Club': {
+        'AV Club Background': LiaHorseHeadSolid,
+        'Guides for Creators': Resources,
+        'Youtube Channel': Youtube,
     },
     'Zcash Social Media': {
         'Podcasts': IosMicrophone,
@@ -116,7 +125,7 @@ const iconsForMenu: IconsFor = {
     "Glossary & FAQ's": {
         'Zcash Library': HiOutlineBuildingLibrary,
         'FAQ': FaQuestion,
-        'gallery': FcGallery
+        'Gallery': FcGallery
     },
     'Contribute': {
         'Help build ZecHub': IoConstructOutline,
