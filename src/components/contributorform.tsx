ea85d244fe31374bb34a8c9b63ed32c1e2768dfe
squirrel <47972284+JJ-Rogers@users.noreply.github.com>
2024-01-01 18:19:15 +0000

    Create contributorform.tsx

---
+++ src/components/contributorform.tsx
@@ -0,0 +1,14 @@
+// Contributorform.tsx
+import React from 'react';
+
+const YourComponent: React.FC = () => (
+  <div>
+    <iframe
+      style={{ border: 'none', width: '100%' }}
+      height="540px"
+      src="https://noteforms.com/forms/zechub-contributor-form"
+    />
+  </div>
+);
+
+export default ContributorForm;
