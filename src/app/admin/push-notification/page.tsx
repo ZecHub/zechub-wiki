d2e27ac6bac61a0eb440d63e7f1925ba23358527
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-01-28 14:21:09 +0100

    feat: add subscriber welcome message comp

--- src/app/admin/push-notification/page.tsx
+++ src/app/admin/push-notification/page.tsx
@@ -1,9 +1,15 @@
 import { ListOfSubscribers } from '@/components/push-notification/ListOfSubscribers/ListOfSubscribers';
+import { SubscriberWelcomeMessage } from '@/components/push-notification/welcome-message/SubscriberWelcomeMessage';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 
 export default withPageAuthRequired(
   async function PushNotificationPage() {
-    return <ListOfSubscribers />;
+    return (
+      <>
+        <SubscriberWelcomeMessage />
+        <ListOfSubscribers />;
+      </>
+    );
   },
   { returnTo: '/admin/push-notification' }
 );
