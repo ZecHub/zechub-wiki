1c6512d41c96b1dab7bcd3997159461c561239e8
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-01-28 14:20:32 +0100

    fix: update body

--- src/components/Admin/AdminComp.tsx
+++ src/components/Admin/AdminComp.tsx
@@ -1,14 +1,16 @@
 'use client';
 
-import './AdminComp.css';
 import { UserProfile } from '@auth0/nextjs-auth0/client';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import './AdminComp.css';
 
 type AdminCompProps = {
   user?: UserProfile | undefined;
 };
 
 export const AdminComp = (props: AdminCompProps) => {
+
   return (
     <>
       <div className='admin'>
@@ -16,15 +18,17 @@ export const AdminComp = (props: AdminCompProps) => {
           <h1>Admin Page</h1>
           <p>Acess to all Admin modules</p>
         </div>
-        <div className='card'>
-          <h3>Push Notification</h3>
-          <div className='cover'>
-            <div className='left'>🔔</div>
-            <div className='right'>
-              <p>Access list of Subscribers</p>
-              <Link href={'/admin/push-notification'} className='btn'>
-                View List
-              </Link>
+        <div className='row'>
+          <div className='card'>
+            <h3>Push Notification</h3>
+            <div className='cover'>
+              <div className='left'>🔔</div>
+              <div className='right'>
+                <p>Access list of Subscribers</p>
+                <Link href={'/admin/push-notification'} className='btn'>
+                  View List
+                </Link>
+              </div>
             </div>
           </div>
         </div>
