0c18389d7e3b5dd4f15a19d1e0cf77fe0a554de2
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-01-29 13:27:21 +0100

    feat: add search to form

--- src/components/push-notification/ListOfSubscribers/ListOfSubscribers.tsx
+++ src/components/push-notification/ListOfSubscribers/ListOfSubscribers.tsx
@@ -27,6 +27,11 @@ export function ListOfSubscribers() {
   const [isLoading, setIsLoading] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [notifyAll, setNotifyAll] = useState(false);
+  const [filteredValue, setFilterdValue] = useState('');
+
+  const filteredData = subscribers.filter((itm) =>
+    itm.id.toLowerCase().includes(filteredValue.toLowerCase())
+  );
 
   const handleGetListOfSubscriber = useCallback(async () => {
     setIsLoading(true);
@@ -76,6 +81,13 @@ export function ListOfSubscribers() {
     }
   };
 
+  const handleFilterOnChange = (e: React.FormEvent<HTMLInputElement>) => {
+    if (e.currentTarget.value) {
+      return;
+    }
+    setFilterdValue(e.currentTarget.value);
+  };
+
   return (
     <>
       <section id='subscribers' className='mt-4 subscribers'>
@@ -87,7 +99,9 @@ export function ListOfSubscribers() {
           <button onClick={handleGetListOfSubscriber}>Get Subscribers</button>
           <input
             type='text'
-            placeholder='Search for by id or endpoint'
+            onChange={handleFilterOnChange}
+            value={filteredValue}
+            placeholder='Search by id or endpoint'
             className='searchForm'
           />
           {subscribers.length > 0 && (
@@ -106,58 +120,57 @@ export function ListOfSubscribers() {
           <div className='loading'>
             <Spinner style={{ width: '32px', height: '32px' }} />
           </div>
-        ) : (
-          subscribers &&
-          subscribers.length > 0 && (
-            <div style={{ overflowX: 'auto' }}>
-              <table className=' my-16 mx-auto border-spacing-1 border border-slate-500'>
-                <thead
-                  style={{
-                    backgroundColor: '#c1c1c1',
-                    height: '42px',
-                  }}
-                >
-                  <tr>
-                    <th>ID</th>
-                    <th style={{ width: '50%' }}>Endpoint</th>
-                    <th>Expiration Time</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {subscribers.map((sub, i) => {
-                    return (
-                      <tr
-                        key={i}
-                        className='shadow-sm hover:bg-gray-200 border-gray-500'
+        ) : subscribers && subscribers.length > 0 ? (
+          <div style={{ overflowX: 'auto' }}>
+            <table className=' my-16 mx-auto border-spacing-1 border border-slate-500'>
+              <thead
+                style={{
+                  backgroundColor: '#c1c1c1',
+                  height: '42px',
+                }}
+              >
+                <tr>
+                  <th>ID</th>
+                  <th style={{ width: '50%' }}>Endpoint</th>
+                  <th>Expiration Time</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredData.map((sub, i) => {
+                  return (
+                    <tr
+                      key={i}
+                      className='shadow-sm hover:bg-gray-200 border-gray-500'
+                    >
+                      <td className='h-1 p-3 m-3 border-spacing-8'>
+                        {wordWrap(sub.id, 10)}
+                      </td>
+                      <td className='h-1 p-3 m-3 border-spacing-8'>
+                        {wordWrap(sub.endpoint, 50)}
+                      </td>
+                      <td className='h-1 p-3 m-3 border-spacing-8'>
+                        {sub.expirationTime || 'Not available'}
+                      </td>
+                      <td
+                        className='h-1 p-3 m-3 border-spacing-8 cursor-pointer'
+                        onClick={() => handleNotifyOne(sub)}
                       >
-                        <td className='h-1 p-3 m-3 border-spacing-8'>
-                          {wordWrap(sub.id, 10)}
-                        </td>
-                        <td className='h-1 p-3 m-3 border-spacing-8'>
-                          {wordWrap(sub.endpoint, 50)}
-                        </td>
-                        <td className='h-1 p-3 m-3 border-spacing-8'>
-                          {sub.expirationTime || 'Not available'}
-                        </td>
-                        <td
-                          className='h-1 p-3 m-3 border-spacing-8 cursor-pointer'
-                          onClick={() => handleNotifyOne(sub)}
-                        >
-                          <span className='tooltip'>
-                            {'🔔'}
-                            <span className='tooltiptext'>
-                              Notify this user
-                            </span>
-                          </span>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          )
+                        <span className='tooltip'>
+                          {'🔔'}
+                          <span className='tooltiptext'>Notify this user</span>
+                        </span>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div className='no-subscribers'>
+            <p>No subscribers at the moment.</p>
+          </div>
         )}
       </section>
       <Modal isOpen={isOpen} handleOnClose={handleOnClose}>
