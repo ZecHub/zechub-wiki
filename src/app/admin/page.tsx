d2e7caa2923675f85f35b172747b09a724e98d18
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-02-05 10:20:46 +0100

    fix: update auth process

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,16 +1,14 @@
 import { authOptions } from '@/auth.config';
 import { AdminComp } from '@/components/Admin/AdminComp';
-import { getServerSession } from 'next-auth/next';
+import { Authentication } from '@/components/Authentication';
+import { getServerSession } from 'next-auth';
 
 export default async function AdminPage() {
-
   const session = await getServerSession(authOptions); // TODO: check for user.role === 'admin'
 
-  if (!session) {
-    return new Response('', {
-      status: 401,
-      statusText: 'You must be logged in.',
-    });
-  }
-  return <AdminComp />;
+  return (
+    <Authentication session={session}>
+      <AdminComp />
+    </Authentication>
+  );
 }
