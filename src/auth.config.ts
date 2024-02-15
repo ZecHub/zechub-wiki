795cfc01de761ae640c6cc87d49a076c1ad58da3
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-02-15 14:54:33 +0100

    fix: prep for Access Control

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,9 +2,23 @@ import { NextAuthOptions } from "next-auth";
 import { NEXTAUTH_SECRET, GITHUB_ID, GITHUB_SECRET } from "./config";
 import GithubProvider from 'next-auth/providers/github';
 
+declare module 'next-auth' {
+  interface User {
+    
+  }
+}
 export const authOptions: NextAuthOptions = {
   secret: NEXTAUTH_SECRET,
-  callbacks: {},
+  callbacks: {
+    session({session, token, user}){
+      return {
+        ...session,
+        user:{
+          ...session.user
+        }
+      }
+    }
+  },
   providers: [
     GithubProvider({
       clientId: GITHUB_ID,
