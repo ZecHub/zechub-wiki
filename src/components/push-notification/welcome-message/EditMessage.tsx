d4825cbb8d81e613b7a9483e530a4a2907da5f81
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-02-18 04:01:06 +0100

    fix: add error tag

--- src/components/push-notification/welcome-message/EditMessage.tsx
+++ src/components/push-notification/welcome-message/EditMessage.tsx
@@ -3,6 +3,7 @@ import {
   handlerUpdateSubscriberWelcomeMessage,
 } from '@/app/actions';
 import { ErrorBoundary } from '@/components/ErrorBoundary/ErrorBoundary';
+import { formatString } from '@/lib/helpers';
 import { useState } from 'react';
 import { experimental_useFormStatus as useFormStatus } from 'react-dom';
 import { useForm } from 'react-hook-form';
@@ -57,10 +58,10 @@ export function EditMessage(props: EditFormProps) {
 
       if (err instanceof ZodError) {
         console.error(err);
-        // toast.error(ErrorToast(err), {
-        //   closeButton: true,
-        //   position: 'bottom-center',
-        // });
+        toast.error(ErrorToast(err), {
+          closeButton: true,
+          position: 'bottom-center',
+        });
       } else {
         toast.error('Failed submitting data', {
           closeButton: true,
@@ -68,6 +69,41 @@ export function EditMessage(props: EditFormProps) {
         });
       }
     }
+  };
+
+  const ErrorToast = (err: any) => {
+    return (
+      <ul className='mb-4'>
+        {err?.errors?.map((e: any, i: number) => (
+          <li key={i} className='mb-4'>
+            <p>
+              Code:{' '}
+              <span className='text-gray-700 text-sm font-bold'>
+                {formatString.removeUnderscore(e.code)}
+              </span>
+            </p>
+            <p>
+              Message:{' '}
+              <span className='text-gray-700 text-sm font-bold'>
+                {e.message}
+              </span>
+            </p>
+            <p>
+              Path:{' '}
+              <span className='text-gray-700 text-sm font-bold'>
+                {e.path[i]}
+              </span>
+            </p>
+            <p>
+              Validation:{' '}
+              <span className='text-gray-700 text-sm font-bold'>
+                {e.validation}
+              </span>
+            </p>
+          </li>
+        ))}
+      </ul>
+    );
   };
 
   return (
@@ -127,18 +163,6 @@ export function EditMessage(props: EditFormProps) {
               })}
               aria-invalid={errors.body ? 'true' : 'false'}
             />
-            {/* <input
-              type='text'
-              id='body'
-              placeholder='Enter description'
-              disabled={isSending}
-              className='w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500'
-              {...register('body', {
-                value: body,
-                required: 'Description is required',
-              })}
-              aria-invalid={errors.body ? 'true' : 'false'}
-            /> */}
             {errors.body?.message && (
               <p role='alert' style={{ color: 'red' }}>
                 {errors.body?.message}
