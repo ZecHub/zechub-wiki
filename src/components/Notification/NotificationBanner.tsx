b4b66e0bf9efa43b353b0215eb5b2c18780da8cd
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-02-19 12:35:58 +0100

    WIP: checking out against deployment error

--- src/components/Notification/NotificationBanner.tsx
+++ src/components/Notification/NotificationBanner.tsx
@@ -11,14 +11,11 @@ export type NotificationBannerProps = {
   };
 };
 
-const customId = 'custom-id-to-prevent-duplicate';
-
+const toastId = 'custom-id-to-prevent-duplicate';
 export const NotificationBanner = (props: NotificationBannerProps) => {
-  const { buttonLabel, description, title, urlRedirectLink } =
-    props.bannerMsg?.data[0];
-
+  // const { buttonLabel, description, title, urlRedirectLink } =
+  //   props.bannerMsg?.data[0];
   const [data, setData] = useState<BannerMessageType>();
-
 
   useEffect(() => {
     async function getData() {
@@ -27,14 +24,33 @@ export const NotificationBanner = (props: NotificationBannerProps) => {
           '/site/toastify-banner-notification/banner-notification.json'
         );
         const data = await res.json();
+        console.log('data: ', data);
         setData(data);
       } catch (err) {
         console.error(err);
+        toast.error(
+          <div className='notification-banner'>
+            <p>{'Error fetching data...'}</p>
+          </div>,
+          {
+            position: 'top-right',
+            autoClose: 8000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            transition: Slide,
+            toastId: 'custom-error-id-to-prevent-duplicate',
+            progressClassName: 'toast-progress-bar',
+            className: 'toast-message',
+          }
+        );
       }
     }
     getData();
 
-    const displayToast = toast(
+    toast(
       <div className='notification-banner'>
         <h1>{data?.title}</h1>
         <p>{data?.description}</p>
@@ -51,16 +67,21 @@ export const NotificationBanner = (props: NotificationBannerProps) => {
         draggable: true,
         progress: undefined,
         transition: Slide,
-        toastId: customId,
+        toastId: toastId,
         progressClassName: 'toast-progress-bar',
         className: 'toast-message',
       }
     );
-  }, [data?.title, data?.buttonLabel, data?.description, data?.urlRedirectLink]);
+  }, [
+    data?.title,
+    data?.buttonLabel,
+    data?.description,
+    data?.urlRedirectLink,
+  ]);
 
   return (
     <>
-      <ToastContainer />
+      <ToastContainer containerId={toastId} />
     </>
   );
 };
