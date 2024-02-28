5745cfeea6df0d5a0b1d5bec72a3cb4637452295
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-02-28 15:30:29 +0100

    fix: commentted fetch api for local file import

--- src/components/Notification/NotificationBanner.tsx
+++ src/components/Notification/NotificationBanner.tsx
@@ -4,40 +4,45 @@ import { formatString } from '@/lib/helpers';
 import { useEffect, useState } from 'react';
 import { Slide, ToastContainer, toast } from 'react-toastify';
 import './NotificationBanner.css';
-
+import contentData from './toastify-banner-content/banner-notification-content.json';
 const toastId = 'custom-id-to-prevent-duplicate';
+
 export const NotificationBanner = () => {
   const [data, setData] = useState<BannerMessageType>();
 
   useEffect(() => {
     async function getData() {
-      try {
-        const res = await fetch(
-          '/site/toastify-banner-notification/banner-notification.json'
-        );
-        const data: BannerMessageType = await res.json();
-        setData(data);
-      } catch (err: any) {
-        console.error(err.message);
-        toast.error(
-          <div className='notification-banner'>
-            <p>{`Error fetching data...`}</p>
-          </div>,
-          {
-            position: 'top-right',
-            autoClose: 8000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            transition: Slide,
-            toastId: 'custom-error-id-to-prevent-duplicate',
-            progressClassName: 'toast-progress-bar',
-            className: 'toast-message',
-          }
-        );
-      }
+      setData(contentData);
+
+      // try {
+      //   const res = await fetch(
+      //     './toastify-banner-notification/banner-notification.json'
+      //   );
+      //   const data: BannerMessageType = await res.json();
+      //   console.log('res: ', res);
+
+      //   setData(data);
+      // } catch (err: any) {
+      //   console.error('err101: ', err);
+      //   toast.error(
+      //     <div className='notification-banner'>
+      //       <p>{`Error fetching data...`}</p>
+      //     </div>,
+      //     {
+      //       position: 'top-right',
+      //       autoClose: 8000,
+      //       hideProgressBar: false,
+      //       closeOnClick: true,
+      //       pauseOnHover: true,
+      //       draggable: true,
+      //       progress: undefined,
+      //       transition: Slide,
+      //       toastId: 'custom-error-id-to-prevent-duplicate',
+      //       progressClassName: 'toast-progress-bar',
+      //       className: 'toast-message',
+      //     }
+      //   );
+      // }
     }
     getData();
 
