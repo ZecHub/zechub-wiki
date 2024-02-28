a415f5e899e320c59abddda4a807668c32c4b271
AliLordLoss <a.a.ghanati@gmail.com>
2024-02-29 00:45:42 +0330

    - added hover effect on wallet tiles, & added link to wallets page from home page > pick a wallet

--- src/components/WalletTile/index.tsx
+++ src/components/WalletTile/index.tsx
@@ -5,8 +5,11 @@ import Image from "next/image";
 
 const WalletTile = ({ name, description, path, image }: WalletProps) => {
   return (
-    <Link href={path} className="w-full h-full inline-block p-2">
-      <div className="h-full border rounded-lg shadow-lg dark:bg-gray-800 p-5">
+    <Link
+      href={path}
+      className="w-full h-full inline-block p-2 hover:-translate-y-3"
+    >
+      <div className="h-full border rounded-lg shadow-lg bg-white dark:bg-gray-800 p-5">
         <div className="flex flex-col items-center justify-center">
           <h5 className="text-xl my-4 font-bold text-blue-700 dark:text-blue-400">
             {name}
