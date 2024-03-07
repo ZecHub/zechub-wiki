1cfaadecc8e14e8b6d9eeb0bc807bdbc55b32d03
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-03-07 12:32:11 +0100

    fix: commented out AppProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,11 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${inter.className} px-12`}>
-          <AppProvider>
+          {/* <AppProvider> */}
             <Navigation />
             <div style={{ margin: '48px 0' }}>{children}</div>
             <Footer />
-          </AppProvider>
+          {/* </AppProvider> */}
       </body>
     </html>
   );
