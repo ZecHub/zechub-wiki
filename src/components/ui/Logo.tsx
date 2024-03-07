1147b976ac03acfa1e3eafb30a81337126c4d65e
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-03-07 12:34:17 +0100

    fix: add priority attribute to Image tag

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image"
+import Image from 'next/image';
 
-const Logo = () =>  (
-    <Image
-        src={"/zechubLogo.jpg"}
-        alt={"Logo"}
-        width={60}
-        height={60}
-        className="rounded-full md:h-50"
-    />      
-)
+const Logo = () => (
+  <Image
+    priority
+    src={'/zechubLogo.jpg'}
+    alt={'Logo'}
+    width={60}
+    height={60}
+    className='rounded-full md:h-50'
+  />
+);
 
-
-export default Logo
+export default Logo;
