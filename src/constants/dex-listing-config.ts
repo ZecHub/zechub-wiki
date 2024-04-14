8bce60e3e106579f7d7e0e323892f5ac051d16a6
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-04-14 18:06:32 +0100

    fix: removed url

--- src/constants/dex-listing-config.ts
+++ src/constants/dex-listing-config.ts
@@ -3,21 +3,21 @@ export const dexListingConfig = [
     title: 'DEX Listings',
     description:
       'Wallets allow you to receive and spend Zcash privately. Some also support encrypted memos',
-    url: 'start-here/new-user-guide',
+    url: ' ',
     image: '/LearnZcash.png',
   },
   {
     title: 'DEX Listings',
     description:
       'Wallets allow you to receive and spend Zcash privately. Some also support ',
-    url: 'wallets',
+    url: ' ',
     image: '/zwallets.png',
   },
   {
     title: 'DEX Listings',
     description:
       'Wallets allow you to receive and spend Zcash privately. Some also support encrypted memos',
-    url: 'guides/using-zec-privately',
+    url: ' ƒ',
     image: '/GuidesZ.png',
   },
 ];
