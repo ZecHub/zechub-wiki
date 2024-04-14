dcf6a9cf4a9fee627ed592eb0c2ed9637170f3cb
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-04-14 17:21:57 +0100

    Revert "Update on the wiki"

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,24 +1,6 @@
 import { exploreFolders } from "@/constants/exploreFolders";
 import Explorer from "@/components/explorer/Explorer";
-import  Metadata from 'next';
 
-
-interface Metadata {
-  title: string;
-  description: string;
-  icons?: string; 
-  ogTitle?: string;
-  ogDescription?: string;
-  ogImage?: string;
-}
-
-export const metadata: Metadata = {
-  title: 'Explore Zcash',
-  description: 'Exploring Zcash',
-  ogTitle: 'Explore Zcash',
-  ogDescription: 'Exploring Zcash',
-  ogImage: 'public/Start-02.png',
-};
 const Explore = async () => {
 
     return (
