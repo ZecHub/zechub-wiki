e71940fa8ba981a29692310a614478132503910a
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-04-19 19:51:57 +0100

    fix: updated body

--- src/app/using-zcash/blockchain-explorers/page.tsx
+++ src/app/using-zcash/blockchain-explorers/page.tsx
@@ -7,14 +7,23 @@ const BlockchainExplorers = () => {
       <h1 className='text-4xl my-12 font-semibold flex justify-center'>
         Blockchain Explorers
       </h1>
+      <p className='text-xl text-center text-slate-600 mb-12'>
+        A blockchain explorer is a search engine that allows users to explore
+        the blockchain of a cryptocurrency. It provides a user-friendly
+        interface to view transaction details, block information, and address
+        histories.
+      </p>
+
       <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
         {blockchainExplorers.map((itm, i) => (
           <Card
-            image={itm.image}
+            thumbnailImage={itm.thumbnailImage}
             description={itm.description}
             title={itm.title}
             url={itm.url}
             key={i}
+            features={itm.features}
+            ctaLabel='Read More'
           />
         ))}
       </div>
