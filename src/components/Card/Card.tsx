f8b37b23c1a43e88fff58a751413a4fb38026274
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-04-20 00:22:35 +0100

    fix: updated hyperlink tag

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -47,6 +47,7 @@ export const Card = (props: CardsProps) => (
     <div className='p-4'>
       <Link
         href={props.url}
+        target='_blank'
         className='inline-flex py-2 px-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
       >
         {props.ctaLabel}
