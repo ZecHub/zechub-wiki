13b9d7218c0460c1c963972aef701be7767afd4a
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-05-07 06:21:23 +0100

    fix: adjusted foreground color

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import dynamic from 'next/dynamic';
+import React from 'react';
 import styles from './Button.module.css';
 
 interface ButtonProps {
@@ -9,7 +9,14 @@ interface ButtonProps {
 
 const DynamicButton: React.FC<ButtonProps> = ({ href, text }) => {
   return (
-    <a href={href} className={styles.button}>
+    <a
+      href={href}
+      className={`${styles.button} ${
+        text == 'Sprout Pool' ? 'hover:text-slate-200' : 'hover:text-black'
+      } font-bold `}
+      target='_blank'
+      rel='external'
+    >
       {text}
     </a>
   );
