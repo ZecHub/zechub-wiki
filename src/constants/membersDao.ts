c60eeea29758405f39217ae081c27ac7a532468a
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-05-09 13:37:03 +0100

    Update membersDao.ts

--- src/constants/membersDao.ts
+++ src/constants/membersDao.ts
@@ -2,7 +2,7 @@ export const daoMembers = [
     {
         imgUrl: 'https://pbs.twimg.com/profile_images/1786133418280980480/4TW1Z3Ye_400x400.jpg',
         name: 'Lowo88',
-        description: `Secret Maxi, Leonine DAO launched and managed 4 DAOs',
+        description: 'Secret Maxi, Leonine DAO launched and managed four DAOs.',
         linkName: 'Twitter',
         urlLink: 'https://twitter.com/LadaleLowo'
     },
