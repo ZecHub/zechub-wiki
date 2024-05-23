ead73cca10756a8294c4f14c4d16fcba3e841853
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-05-23 15:24:14 +0100

    Update page.tsx

--- src/app/using-zcash/non-custodial-exchanges/page.tsx
+++ src/app/using-zcash/non-custodial-exchanges/page.tsx
@@ -5,7 +5,7 @@ const DEXListingPage = () => {
   return (
     <div className='container mx-auto px-4'>
       <h1 className='text-4xl my-12 font-semibold flex justify-center'>
-        Decentralize Exchange Listing
+        Non-Custodial Exchanges 
       </h1>
 
       <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
