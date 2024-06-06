c5c9a67e2a563895ffa442a6302f8997fa1f1ca9
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-06-06 21:28:11 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 };
 
 
-export default async function Home() {
+// export default async function Home() {
   // const bannerMsg = await getBannerMessage();
 
   const text = `ZecHub is the community driven education hub for the Zcash cryptocurrency (ZEC). Zcash is a digital currency providing censorship resistant, secure & private payments. The Zcash Blockchain utilises highly advanced 'verifiable' zk-snarks that do not require Trusted Setup following the NU5 network upgrade in 2022.`;
