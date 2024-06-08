8909d0b930ec663bf0a9d3160eb3a0d08b19775d
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-06-08 15:01:52 +0100

    Update page.tsx

--- src/app/dao/page.tsx
+++ src/app/dao/page.tsx
@@ -1,130 +1,110 @@
-'use client';
-import { FadeInAnimation } from '@/components/ui/FadeInAnimation';
-import MemberCards from '@/components/ui/MemberCards';
-import { daoMembers } from '@/constants/membersDao';
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
-const DaoMembers = () => {
+interface DaoProps {
+  imgUrl: string;
+  name: string;
+  description: string;
+  linkName: string;
+  urlLink: string;
+  messageLink: string;
+}
+
+const MemberCards = ({
+  imgUrl,
+  name,
+  description,
+  linkName,
+  urlLink,
+  messageLink,
+}: DaoProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [shortDescription, setShortDescription] = useState(description);
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [message, setMessage] = useState('');
+  const shouldShowReadMore = description.length >= 37;
+
+  useEffect(() => {
+    if (description.length >= 37) {
+      setShortDescription(description.slice(0, 32) + "...");
+    }
+  }, [description]);
+
   return (
-    <main>
-      <FadeInAnimation>
-        <h1 className='text-4xl my-12 font-semibold flex justify-center'>
-          ZecHub DAO
-        </h1>
+    <div className="card-container m-2.5">
+      <div className={`card border p-5 rounded-lg shadow-lg dark:bg-gray-800 ${isFlipped ? 'flipped' : ''}`}>
+        <div className="card-front flex flex-col items-center justify-center">
+          <Image
+            className="w-50 h-50 my-3 rounded-full shadow-lg"
+            src={imgUrl ? imgUrl : ""}
+            alt={`${name} profile image`}
+            width={200}
+            height={200}
+            loading="lazy"
+          />
+          <h5 className="text-xl my-4 font-bold text-gray-900 dark:text-white">
+            {name}
+          </h5>
+          <div className="p-4">
+            <p className={`text-gray-600 ${isOpen ? "hidden" : "line-clamp-3"}`}>
+              {shortDescription}
+            </p>
+            {shouldShowReadMore && (
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="text-blue-500 mt-2 focus:outline-none"
+              >
+                <span>{isOpen ? "Read Less" : "Read More"}</span>
+              </button>
+            )}
 
-        <p className='text-xl text-center text-slate-600 mb-12'>
-         ZecHub provides an open source educational platform where community members can work together on creating, validating, and promoting content that supports the Zcash ecosystem. ZecHub believes:
-         Privacy is a human right
-         Education should be open-source and accessible worldwide
-         Community members have a right to earn ZEC privately
-
-          ZecHub DAO is comprised of likeminded individuals located around the world with the common goal of effectively guiding Zcash and Privacy Technology education. 
-        </p>
-      </FadeInAnimation>
-
-<FadeInAnimation>
-  <h3 className='text-3xl mb-4 font-semibold'>
-    What makes ZecHub unique?
-  </h3>
-
-  <p className='text-base text-slate-600 mb-12'>
-    ZecHub is one of the first public DAO&apos;s in Zcash&apos;s history.
-    Its funds are held primarily in a Shielded Zcash address, compared to most DAO&apos;s that live EVM chains such as Ethereum.
-  </p>
-</FadeInAnimation>
-      <FadeInAnimation>
-        <h3 className='text-3xl mb-4 font-semibold'>Governance</h3>
-
-        <p className='text-base text-slate-600 mb-12'>
-          All DAO proposals are public and can be viewed{' '}
-          <a href='https://vote.zechub.xyz/'>here</a>. Anyone can create a
-          proposal to vote on. In an effort to support community transparency,
-          the ZecHubDAO will post all governance proposals in the Zcash
-          Community Forum{' '}
-          <a href='https://forum.zcashcommunity.com/t/zechub-governance-updates/43674'>
-            ZecHub Governance thread
-          </a>
-          .
-        </p>
-      </FadeInAnimation>
-
-      <div className='my-12'>
-        <h3 className='text-3xl mb-4 font-semibold text-center'>DAO Members</h3>
-
-        <div className='w-full grid grid-cols-1 space-x-2 md:grid-cols-3 md:gap-4 justify-items-center  mt-4 p-2'>
-          {daoMembers &&
-            daoMembers.map((e) => (
-              <FadeInAnimation key={e.name}>
-                <div className='flex justify-center space-y-4 w-full space-x-3 md:space-y-2 '>
-                  <MemberCards
-                    imgUrl={e.imgUrl}
-                    description={e.description}
-                    name={e.name}
-                    linkName={e.linkName}
-                    urlLink={e.urlLink}
-                  />
-                </div>
-              </FadeInAnimation>
-            ))}
+            {isOpen && <p className="text-gray-600 mt-2">{description}</p>}
+          </div>
+          <div className="inherit mt-4 w-full justify-center text-center items-center md:mt-6">
+            <Link
+              href={urlLink}
+              target="_blank"
+              className="inline-flex items-center w-1/2 justify-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              {linkName}
+            </Link>
+            <button
+              onClick={() => setIsFlipped(true)}
+              className="inline-flex items-center w-1/2 justify-center px-4 py-2 mt-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Message
+            </button>
+          </div>
+        </div>
+        <div className="card-back p-5 dark:bg-gray-800">
+          <textarea
+            className="w-full p-2 border rounded-lg"
+            rows="4"
+            placeholder="Type your message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              // Handle the send message logic here
+              setIsFlipped(false); // Flip back after sending the message
+            }}
+            className="mt-4 inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          >
+            Send
+          </button>
+          <button
+            onClick={() => setIsFlipped(false)}
+            className="mt-2 inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+          >
+            Cancel
+          </button>
         </div>
       </div>
-
-      <FadeInAnimation>
-        <h3 className='text-3xl mb-4 font-semibold'>Website Links</h3>
-        <ul className='text-base text-slate-600 mb-12 list-disc px-4'>
-          <li>
-            <a
-              target='_blank'
-              className='text-blue-500 active:text-blue-700 hover:text-blue-600'
-              href='https://daodao.zone/dao/juno1nktrulhakwm0n3wlyajpwxyg54n39xx4y8hdaqlty7mymf85vweq7m6t0y/proposals'
-            >
-              DAODAO
-            </a>
-          </li>
-          <li>
-            <a
-              target='_blank'
-              className='text-blue-500 active:text-blue-700 hover:text-blue-600'
-              href='https://snapshot.org/#/zechubdao.eth'
-            >
-              Ethereum DAO
-            </a>
-          </li>
-        </ul>
-      </FadeInAnimation>
-      <div>
-        <h3 className='text-3xl mb-4 font-semibold'>Sources</h3>
-        <ul className='text-base text-slate-600 mb-12 list-disc px-4'>
-          <li>
-            <a
-              target='_blank'
-              className='text-blue-500 active:text-blue-700 hover:text-blue-600'
-              href='https://twitter.com/ZecHub/status/1569827000218537984'
-            >
-              https://twitter.com/ZecHub/status/1569827000218537984?s=20&t=v6h3n3P7o7LMbnAG-O8Kug
-            </a>
-          </li>
-          <li>
-            <a
-              target='_blank'
-              className='text-blue-500 active:text-blue-700 hover:text-blue-600'
-              href='https://forum.zcashcommunity.com/t/zechub-rfi/42778/17'
-            >
-              https://forum.zcashcommunity.com/t/zechub-rfi/42778/17
-            </a>
-          </li>
-          <li>
-            <a
-              target='_blank'
-              className='text-blue-500 active:text-blue-700 hover:text-blue-600'
-              href='https://www.forbes.com/sites/cathyhackl/2021/06/01/what-are-daos-and-why-you-should-pay-attention/?sh=3d34fb2c7305'
-            >
-              https://www.forbes.com/sites/cathyhackl/2021/06/01/what-are-daos-and-why-you-should-pay-attention/?sh=3d34fb2c7305
-            </a>
-          </li>
-        </ul>
-      </div>
-    </main>
+    </div>
   );
 };
 
-export default DaoMembers;
+export default MemberCards;
