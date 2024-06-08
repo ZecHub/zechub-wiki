5d24c3c5c8a48b24bb6b659e422b11aa1750de1a
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-06-08 16:13:05 +0100

    Update MemberCards.tsx

--- src/components/ui/MemberCards.tsx
+++ src/components/ui/MemberCards.tsx
@@ -40,21 +40,21 @@ const MemberCards = ({
   };
 
   return (
-    <div className="card-container m-2.5">
+    <div className="card-container m-4">
       <div className={`card border p-5 rounded-lg shadow-lg dark:bg-gray-800 ${isFlipped ? 'flipped' : ''}`}>
         <div className="card-front flex flex-col items-center justify-center">
           <Image
-            className="w-50 h-50 my-3 rounded-full shadow-lg"
+            className="w-32 h-32 my-3 rounded-full shadow-lg"
             src={imgUrl ? imgUrl : ""}
             alt={`${name} profile image`}
-            width={200}
-            height={200}
+            width={128}
+            height={128}
             loading="lazy"
           />
           <h5 className="text-xl my-4 font-bold text-gray-900 dark:text-white">
             {name}
           </h5>
-          <div className="p-4">
+          <div className="p-4 text-center">
             <p className={`text-gray-600 ${isOpen ? "hidden" : "line-clamp-3"}`}>
               {shortDescription}
             </p>
@@ -69,17 +69,17 @@ const MemberCards = ({
 
             {isOpen && <p className="text-gray-600 mt-2">{description}</p>}
           </div>
-          <div className="inherit mt-4 w-full justify-center text-center items-center md:mt-6">
+          <div className="mt-4 w-full flex justify-center items-center md:mt-6 space-x-2">
             <Link
               href={urlLink}
               target="_blank"
-              className="inline-flex items-center w-1/2 justify-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               {linkName}
             </Link>
             <button
               onClick={() => setIsFlipped(true)}
-              className="inline-flex items-center w-1/2 justify-center px-4 py-2 mt-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Message
             </button>
@@ -88,7 +88,7 @@ const MemberCards = ({
         <div className="card-back p-5 dark:bg-gray-800 relative">
           <textarea
             className="w-full p-2 border rounded-lg"
-            rows={4} // Ensure this is a number
+            rows={4}
             placeholder="Type your message..."
             maxLength={512}
             value={message}
@@ -99,13 +99,13 @@ const MemberCards = ({
           </div>
           <button
             onClick={handleSend}
-            className="mt-4 inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            className="mt-4 inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-green-700 rounded-lg hover:bg-green-800 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
           >
             Send
           </button>
           <button
             onClick={() => setIsFlipped(false)}
-            className="mt-2 inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+            className="mt-2 inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-red-700 rounded-lg hover:bg-red-800 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
           >
             Cancel
           </button>
@@ -117,7 +117,7 @@ const MemberCards = ({
 
 export default MemberCards;
 
-function base64UrlEncode(str: string) { 
+function base64UrlEncode(str: string) {
   return btoa(str)
     .replace(/\+/g, '-')
     .replace(/\//g, '_')
