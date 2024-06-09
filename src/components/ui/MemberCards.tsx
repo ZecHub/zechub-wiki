5ad0854ef1e4d44d734540b98bea5dde11b3e96d
MANUEL RIOS <m.rios56@hotmail.com>
2024-06-09 00:48:57 +0000

    added flip cards

--- src/components/ui/MemberCards.tsx
+++ src/components/ui/MemberCards.tsx
@@ -115,7 +115,7 @@ const MemberCards = ({
             }}
             className="mt-2 inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
           >
-            Cancel
+            Cancel 
           </button>
         </div>
       </div>
