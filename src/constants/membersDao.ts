41024deddc99b03c68ac87a6b6475de95522756e
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-06-09 16:02:57 +0100

    Update membersDao.ts

--- src/constants/membersDao.ts
+++ src/constants/membersDao.ts
@@ -1,47 +1,5 @@
 export const daoMembers = [
     {
-        imgUrl: 'https://pbs.twimg.com/profile_images/1786133418280980480/4TW1Z3Ye_400x400.jpg',
-        name: 'Lowo88',
-        description: 'Secret Maxi, Leonine DAO launched and managed four DAOs.',
-        linkName: 'Twitter',
-        urlLink: 'https://twitter.com/LadaleLowo',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
-    },
-    {
-        imgUrl: 'https://pbs.twimg.com/profile_images/1756041822973882368/oax4ppkd_400x400.jpg',
-        name: 'zerodartz',
-        description: `Meme and media creator, ZFAV club contributor, nerd who loves new technology and Shielding everything`,
-        linkName: 'Twitter',
-        urlLink: 'https://twitter.com/zerodartz',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
-    },
-    {
-        imgUrl: 'https://i.ibb.co/rbMZ67Y/profile.jpg',
-        name: 'zksquirrel',
-        description: `Core Contributor on ZecHub and Community Note Taker for Zcash Arborist
-        and development of the Zcash ecosystem.`,
-        linkName: 'Twitter',
-        urlLink: 'https://twitter.com/zksquirrel',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
-    },
-    {
-        imgUrl: 'https://i.ibb.co/kQk5sG7/image-2023-11-17-170857858.png',
-        name: 'Tecnopapapi',
-        description: 'Father, Journalist, Blogger and Podcaster from Venezuela. I like to learn and reach others about computers, smartphones and crypto as a financial alternative',
-        linkName:'Twitter',
-        urlLink: 'https://twitter.com/tecnopapapi',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
-    },
-    {
-        imgUrl: 'https://sea2.discourse-cdn.com/zcash/user_avatar/forum.zcashcommunity.com/dismad/120/18432_2.png',
-        name: 'Dismad',
-        description: `Education, Skateboarding, Mathematics, Computer Science, Ornithology, Meteorology, Music, Automotive,
-        Supercross, Cryptocurrencies`,
-        linkName:'Zcash',
-        urlLink: 'https://zcashblockexplorer.com/ua/u1zhgy24tweexhjcsstya5qqzrus4cgv0amasfv5jp6f3p3qvw265rugn8ref5djg472l5s382mwuffremffr7se6xjlh5exagwg2d6frs',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
-    },
-    {
         imgUrl: 'https://prozcash.ru/wp-content/uploads/2023/04/serdcze.jpeg',
         name: 'RuZcash',
         description: `I'm the creator of prozcash.ru website, which I fill with translations into Russian of press releases from
@@ -50,65 +8,23 @@ export const daoMembers = [
         about Zcash.`,
         linkName:'Twitter',
         urlLink: 'https://twitter.com/RuZcash',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
+        zcashAddress: '#'
     },
     {
-        imgUrl: 'https://pbs.twimg.com/profile_images/1703030858771279872/1onKu0vX_400x400.jpg',
-        name: 'Roosevelt Gordones',
-        description: `Zcast Podcast en Español, Educator & Media Production.`,
-        linkName:'Twitter',
-        urlLink: 'https://twitter.com/gordonesTV',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
+        imgUrl: 'https://i.ibb.co/q5p0YPr/lowo.jpg',
+        name: 'Lowo88',
+        description: 'Secret Maxi, Leonine DAO launched and managed four DAOs.',
+        linkName: 'Twitter',
+        urlLink: 'https://twitter.com/LadaleLowo',
+        zcashAddress: 'u1xut04cg3v0a5zqpud5l5nrkx8x4847q2fg38jzw8840aqj6e6gzj6m4paae906h88dsehymz5m28lrdfgwcvkymhqpfk0hlmjx2gef08q3rty9ljvnvxv9fevggegzqeql63dl220kpwe4n60kzwz87eq7mj8ppsuhqhvdzhdp00dymfgrc05e73ljwagn6ct5t33nrel0n7gxftv29'
     },
     {
-        imgUrl: 'https://pbs.twimg.com/profile_images/1560452481829818368/c6XtZG6i_200x200.jpg',
-        name: 'Michae2xl',
-        description: `1 year+ as Zcash Brazil ambassador, Software Engineer & Product Owner, known as Michae2xl on social media, has
-        been 7+ years in the crypto space, worked for a DAO as a Community Manager in Brazil & Canada for almost 4
-        years, and built an NFT index, good expertise as a leader to building communities.`,
-        linkName:'Twitter',
-        urlLink: 'https://twitter.com/michae2xl',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
-    },
-    {
-        imgUrl: 'https://pbs.twimg.com/profile_images/1699907348993626112/KRH0sBXD_400x400.jpg',
-        name: 'Decentralistdan',
-        description: 'ᙇecosystem relations @ZcashFoundation',
-        linkName:'Twitter',
-        urlLink: 'https://twitter.com/decentralistdan',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
-    },
-    {
-        imgUrl: 'https://pbs.twimg.com/profile_images/1528027327921201152/ra7kpu96_400x400.jpg',
-        name: 'Jason McGee',
-        description: `Member of Zcash Community Grants Committe and Founder of Shielded Labs.`,
-        linkName:'Twitter',
-        urlLink: 'https://twitter.com/aquietinvestor',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
-    },
-    {
-        imgUrl: 'https://pbs.twimg.com/profile_images/1634614829607538694/7_mFB6_T_400x400.jpg',
-        name: 'Chidi',
-        description: `Zcash Nigeria Global Ambassador.`,
-        linkName:'Twitter',
-        urlLink: 'https://twitter.com/ZcashNigeria',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
-    },
-    {
-        imgUrl: 'https://i.ibb.co/gDg75Px/upscale-245339439045212-663968106.png',
-        name: 'Jay',
-        description: `Privacy, Cryptography and Encryption.`,
-        linkName:'Contact',
-        urlLink: '#',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
-    },
-    {
-        imgUrl: 'https://pbs.twimg.com/profile_images/1560475487004250112/GoLrrEim_400x400.jpg',
-        name: 'vito',
-        description: `Data Security Analyst & Community Manager at Zcash Brasil.`,
-        linkName:'Twitter',
-        urlLink: 'https://twitter.com/vitozkp',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
+        imgUrl: 'https://pbs.twimg.com/profile_images/1756041822973882368/oax4ppkd_400x400.jpg',
+        name: 'zerodartz',
+        description: `Meme and media creator, ZFAV club contributor, nerd who loves new technology and Shielding everything`,
+        linkName: 'Twitter',
+        urlLink: 'https://twitter.com/zerodartz',
+        zcashAddress: 'u1wxl376kqe5vvr0e594xrsuq4d59ztxvk74gx9vnmyvp49sslueqzqtnv6a437c4z2t7t0zklmjducmxcsghzyne45crhn2k0fnf493473qgak4350d5hcjses6tkuxdkry09wlvq0tyjk4jmca6t54vjh4sgtf5cpcq0g90gk8eqvxkmg96euegpvprca6usuwf5znh7q5w967adnep'
     },
     {
         imgUrl: 'https://free2z.cash/uploadz/public/Edicksonjga/IMG_20230415_142522_715.jpg',
@@ -117,7 +33,7 @@ export const daoMembers = [
         community on Telegram and YouTube channel, Editor of zcashesp.com/blog and Contributor of ZecHub.`,
         linkName:'Telegram',
         urlLink: 'https://t.me/Edicksonjga',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
+        zcashAddress: 'u19cd4nhq2l0gpn985h8d63xkqj2man6gpzm8fkjczzy7lkgaermgv5ueuyp86r4eemwef0x4mavg9mmuycxcar3x477mrp98p97x7l3qvzrut6ap4y9c8xuy7hcq8q90ek6rhkd3xlufshjyv7dzpv4mx428pzq568jrye77hratw4tjwdwqekqljgyalnm8l3y72sqdd9jed6k65vpf'
     },
     {
         imgUrl: 'https://pbs.twimg.com/profile_images/1568361509104803840/KFrdM4OY_200x200.jpg',
@@ -129,7 +45,7 @@ export const daoMembers = [
         withmy work.`,
         linkName:'Twitter',
         urlLink: 'https://twitter.com/taminevg',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
+        zcashAddress: '#'
     },
     {
         imgUrl: 'https://i.seadn.io/gcs/files/b7d75113578335f503d620166f459b4a.png',
@@ -138,7 +54,92 @@ export const daoMembers = [
         programming and has mentored hundreds of junior developers at a coding bootcamp.`,
         linkName:'Twitter',
         urlLink: 'https://twitter.com/luckytokidoki',
+        zcashAddress: '#'
+    },
+    {
+        imgUrl: 'https://i.ibb.co/kQk5sG7/image-2023-11-17-170857858.png',
+        name: 'Tecnopapapi',
+        description: 'Father, Journalist, Blogger and Podcaster from Venezuela. I like to learn and reach others about computers, smartphones and crypto as a financial alternative',
+        linkName:'Twitter',
+        urlLink: 'https://twitter.com/tecnopapapi',
+        zcashAddress: 'u1sa0tnr9ac7ul8ad3ftdk4pgcxuq8j9vagjwz7apf8lylryhhklu4j68745trlga62a68qtfaqllmmdpv6zghw560qgzgughejre4ekmqvzf6agtu82a8qx2s3nj3gu50ye6r06rce595rvp0k4t28ecn5kzqu53q99guuzmrej94hr4u6l34rctvwsyzdhtq2ap070qrnvcwg4tgj66'
+    },
+    {
+        imgUrl: 'https://sea2.discourse-cdn.com/zcash/user_avatar/forum.zcashcommunity.com/dismad/120/18432_2.png',
+        name: 'Dismad',
+        description: `Education, Skateboarding, Mathematics, Computer Science, Ornithology, Meteorology, Music, Automotive,
+        Supercross, Cryptocurrencies`,
+        linkName:'Zcash',
+        urlLink: 'https://zcashblockexplorer.com/ua/u1zhgy24tweexhjcsstya5qqzrus4cgv0amasfv5jp6f3p3qvw265rugn8ref5djg472l5s382mwuffremffr7se6xjlh5exagwg2d6frs',
+        zcashAddress: 'u1zhgy24tweexhjcsstya5qqzrus4cgv0amasfv5jp6f3p3qvw265rugn8ref5djg472l5s382mwuffremffr7se6xjlh5exagwg2d6frs'
+    },
+    {
+        imgUrl: 'https://pbs.twimg.com/profile_images/1703030858771279872/1onKu0vX_400x400.jpg',
+        name: 'Roosevelt Gordones',
+        description: `Zcast Podcast en Español, Educator & Media Production.`,
+        linkName:'Twitter',
+        urlLink: 'https://twitter.com/gordonesTV',
+        zcashAddress: 'u1nvhfur5lruk4lzy7v5j4wqs65rvslk7tvgxvcjpe7f8u08vjs98rsg9sralpnwgzzkevtr6k2flh7g9hqqk07m9eff222ljyg9ge2d7upx9ycr9lqzmwmww05npwypmw5t2cvt32tnuylnud4qts3hq2af5w50t3czatklrv5f7spwpfasjdr3rm7qpklpmvv5fc5vpl2spyysvvwra'
+    },
+    {
+        imgUrl: 'https://pbs.twimg.com/profile_images/1560452481829818368/c6XtZG6i_200x200.jpg',
+        name: 'Michae2xl',
+        description: `1 year+ as Zcash Brazil ambassador, Software Engineer & Product Owner, known as Michae2xl on social media, has
+        been 7+ years in the crypto space, worked for a DAO as a Community Manager in Brazil & Canada for almost 4
+        years, and built an NFT index, good expertise as a leader to building communities.`,
+        linkName:'Twitter',
+        urlLink: 'https://twitter.com/michae2xl',
+        zcashAddress: 'x'
+    },
+    {
+        imgUrl: 'https://pbs.twimg.com/profile_images/1699907348993626112/KRH0sBXD_400x400.jpg',
+        name: 'Decentralistdan',
+        description: 'ᙇecosystem relations @ZcashFoundation',
+        linkName:'Twitter',
+        urlLink: 'https://twitter.com/decentralistdan',
+        zcashAddress: 'x'
+    },
+    {
+        imgUrl: 'https://pbs.twimg.com/profile_images/1528027327921201152/ra7kpu96_400x400.jpg',
+        name: 'Jason McGee',
+        description: `Member of Zcash Community Grants Committe and Founder of Shielded Labs.`,
+        linkName:'Twitter',
+        urlLink: 'https://twitter.com/aquietinvestor',
+        zcashAddress: 'u102x7c4x8fyj05x3ja8psl0cet0vn8exwt49qxsshq2l2egt4u9qyq2374vf7f2t2d8w6yukykjh0lupyr28ctxxuvums4gzgfyjnys5y'
+    },
+    {
+        imgUrl: 'https://pbs.twimg.com/profile_images/1634614829607538694/7_mFB6_T_400x400.jpg',
+        name: 'Chidi',
+        description: `Zcash Nigeria Global Ambassador.`,
+        linkName:'Twitter',
+        urlLink: 'https://twitter.com/ZcashNigeria',
+        zcashAddress: '#'
+    },
+    {
+        imgUrl: 'https://i.ibb.co/gDg75Px/upscale-245339439045212-663968106.png',
+        name: 'Jay',
+        description: `Privacy, Cryptography and Encryption.`,
+        linkName:'Contact',
+        urlLink: '#',
+        zcashAddress: '#'
+    },
+    {
+        imgUrl: 'https://pbs.twimg.com/profile_images/1560475487004250112/GoLrrEim_400x400.jpg',
+        name: 'vito',
+        description: `Data Security Analyst & Community Manager at Zcash Brasil.`,
+        linkName:'Twitter',
+        urlLink: 'https://twitter.com/vitozkp',
+        zcashAddress: '#'
+    },
+    {
+        imgUrl: 'https://i.ibb.co/rbMZ67Y/profile.jpg',
+        name: 'zksquirrel',
+        description: `Core Contributor on ZecHub and Community Note Taker for Zcash Arborist
+        and development of the Zcash ecosystem.`,
+        linkName: 'Twitter',
+        urlLink: 'https://twitter.com/zksquirrel',
         zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
     },
+
 
 ]
