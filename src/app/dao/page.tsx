654cddd01f6c9ae0f671ba5c578768be32296b6f
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-06-10 20:16:29 +0100

    Update page.tsx

--- src/app/dao/page.tsx
+++ src/app/dao/page.tsx
@@ -1,17 +1,30 @@
 'use client';
 import React from 'react';
+import Image from 'next/image';
 import { FadeInAnimation } from '@/components/ui/FadeInAnimation';
 import MemberCards from '@/components/ui/MemberCards';
 import { daoMembers } from '@/constants/membersDao';
+import logo from '/public/DAOlogo.png';
+import daoLogo from '/public/daodao.png'; 
+import ethLogo from '/public/snapshot.jpg'; 
 
 const DaoMembers: React.FC = () => {
   return (
     <main>
       <FadeInAnimation>
-        <h1 className='text-4xl my-12 font-semibold flex justify-center'>
-          ZecHub DAO
-        </h1>
-
+        <div className="flex items-center justify-center my-12">
+          <Image 
+            src={logo} 
+            alt="ZecHub Logo" 
+            width={50} 
+            height={50} 
+            className="mr-4"
+          />
+          <h1 className='text-4xl font-semibold'>
+            ZecHub DAO
+          </h1>
+        </div>
+        
         <p className='text-xl text-center text-slate-600 mb-12'>
           ZecHub provides an open source educational platform where community members can work together on creating, validating, and promoting content that supports the Zcash ecosystem. ZecHub believes:
           Privacy is a human right
@@ -43,9 +56,27 @@ const DaoMembers: React.FC = () => {
           Community Forum{' '}
           <a href='https://forum.zcashcommunity.com/t/zechub-governance-updates/43674'>
             ZecHub Governance thread
-          </a>
-          .
+          </a>.
         </p>
+
+        <div className='flex justify-center space-x-4 mb-12'>
+          <a href='https://daodao.zone/dao/juno1nktrulhakwm0n3wlyajpwxyg54n39xx4y8hdaqlty7mymf85vweq7m6t0y/proposals' target='_blank'>
+            <Image 
+              src={daoLogo} 
+              alt="DAODAO" 
+              width={50} 
+              height={50}
+            />
+          </a>
+          <a href='https://snapshot.org/#/zechubdao.eth' target='_blank'>
+            <Image 
+              src={ethLogo} 
+              alt="Ethereum DAO" 
+              width={50} 
+              height={50} 
+            />
+          </a>
+        </div>
       </FadeInAnimation>
 
       <div className='my-12'>
@@ -53,44 +84,20 @@ const DaoMembers: React.FC = () => {
 
         <div className='grid-container'>
           {daoMembers &&
-            daoMembers.map((member) => (
-              <FadeInAnimation key={member.name}>
+            daoMembers.map((e) => (
+              <FadeInAnimation key={e.name}>
                 <MemberCards
-                  imgUrl={member.imgUrl}
-                  description={member.description}
-                  name={member.name}
-                  linkName={member.linkName}
-                  urlLink={member.urlLink}
-                  zcashAddress={member.zcashAddress}
+                  imgUrl={e.imgUrl}
+                  description={e.description}
+                  name={e.name}
+                  linkName={e.linkName}
+                  urlLink={e.urlLink}
+                  zcashAddress={e.zcashAddress}
                 />
               </FadeInAnimation>
             ))}
         </div>
       </div>
-
-      <FadeInAnimation>
-        <h3 className='text-3xl mb-4 font-semibold'>Website Links</h3>
-        <ul className='text-base text-slate-600 mb-12 list-disc px-4'>
-          <li>
-            <a
-              target='_blank'
-              className='text-blue-500 active:text-blue-700 hover:text-blue-600'
-              href='https://daodao.zone/dao/juno1nktrulhakwm0n3wlyajpwxyg54n39xx4y8hdaqlty7mymf85vweq7m6t0y/proposals'
-            >
-              DAODAO
-            </a>
-          </li>
-          <li>
-            <a
-              target='_blank'
-              className='text-blue-500 active:text-blue-700 hover:text-blue-600'
-              href='https://snapshot.org/#/zechubdao.eth'
-            >
-              Ethereum DAO
-            </a>
-          </li>
-        </ul>
-      </FadeInAnimation>
 
       <div>
         <h3 className='text-3xl mb-4 font-semibold'>Sources</h3>
