4cdb7e6019d37335d409f33d6f6a531701a44207
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-06-12 18:24:13 +0100

    Update page.tsx

--- src/app/dao/page.tsx
+++ src/app/dao/page.tsx
@@ -20,12 +20,10 @@ const DaoMembers: React.FC = () => {
             height={100}
             className="mr-4"
           />
-          <h1 className='text-4xl font-semibold'>
-            ZecHub DAO
-          </h1>
+          <h1 className="text-4xl font-semibold">ZecHub DAO</h1>
         </div>
 
-        <p className='text-xl text-center text-slate-600 mb-12'>
+        <p className="text-xl text-center text-slate-600 mb-12">
           ZecHub provides an open source educational platform where community members can work together on creating, validating, and promoting content that supports the Zcash ecosystem. ZecHub believes:
           Privacy is a human right
           Education should be open-source and accessible worldwide
@@ -35,55 +33,52 @@ const DaoMembers: React.FC = () => {
       </FadeInAnimation>
 
       <FadeInAnimation>
-        <h3 className='text-3xl mb-4 font-semibold'>
-          What makes ZecHub unique?
-        </h3>
+        <h3 className="text-3xl mb-4 font-semibold">What makes ZecHub unique?</h3>
 
-        <p className='text-base text-slate-600 mb-12'>
-          ZecHub is the first Zcash ecosystem DAO. Its funds are held primarily within the Zcash shielded pool. Global Ambassadors, Community Grants members and community experts form a group that decides the direction of Zcash education. The DAO also allows any group to create SubDAO's which are able to form projects and create proposals or request Retroactive Compensation via the DAO DAO module. 
+        <p className="text-base text-slate-600 mb-12">
+          ZecHub is the first Zcash ecosystem DAO. Its funds are held primarily within the Zcash shielded pool. Global Ambassadors, Community Grants members and community experts form a group that decides the direction of Zcash education. The DAO also allows any group to create SubDAO's which are able to form projects and create proposals or request Retroactive Compensation via the DAO DAO module.
         </p>
       </FadeInAnimation>
 
       <FadeInAnimation>
-        <h3 className='text-3xl mb-4 font-semibold'>Governance</h3>
+        <h3 className="text-3xl mb-4 font-semibold">Governance</h3>
 
-        <p className='text-base text-slate-600 mb-12'>
+        <p className="text-base text-slate-600 mb-12">
           All DAO proposals are public and can be viewed using the DAO DAO or Snapshot buttons. Anyone can create a proposal to vote on. In an effort to support community transparency, ZecHub DAO will post all governance proposals in the Zcash Community Forum ZecHub Governance thread.
-          </a>.
         </p>
 
-        <div className='flex justify-center items-center text-center space-x-4 mb-12'>
+        <div className="flex justify-center items-center text-center space-x-4 mb-12">
           <div>
-            <a href='https://daodao.zone/dao/juno1nktrulhakwm0n3wlyajpwxyg54n39xx4y8hdaqlty7mymf85vweq7m6t0y/proposals' target='_blank'>
+            <a href="https://daodao.zone/dao/juno1nktrulhakwm0n3wlyajpwxyg54n39xx4y8hdaqlty7mymf85vweq7m6t0y/proposals" target="_blank">
               <Image
-                className='rounded-full border-4 border-slate-400 dark:border-black hover:scale-125'
+                className="rounded-full border-4 border-slate-400 dark:border-black hover:scale-125"
                 src={daoLogo}
                 alt="DAODAO"
                 width={80}
                 height={80}
               />
             </a>
-            <h1 className='font-bold'>DAO DAO</h1>
+            <h1 className="font-bold">DAO DAO</h1>
           </div>
           <div>
-            <a href='https://snapshot.org/#/zechubdao.eth' target='_blank'>
+            <a href="https://snapshot.org/#/zechubdao.eth" target="_blank">
               <Image
-                className='rounded-full border-4 border-slate-400 dark:border-black hover:scale-125'
+                className="rounded-full border-4 border-slate-400 dark:border-black hover:scale-125"
                 src={ethLogo}
                 alt="Ethereum DAO"
                 width={80}
                 height={80}
               />
             </a>
-            <h1  className='font-bold'> Snapshot</h1>
+            <h1 className="font-bold">Snapshot</h1>
           </div>
         </div>
       </FadeInAnimation>
 
-      <div className='my-12'>
-        <h3 className='text-3xl mb-4 font-semibold text-center'>DAO Members</h3>
+      <div className="my-12">
+        <h3 className="text-3xl mb-4 font-semibold text-center">DAO Members</h3>
 
-        <div className='grid-container'>
+        <div className="grid-container">
           {daoMembers &&
             daoMembers.map((e) => (
               <FadeInAnimation key={e.name}>
@@ -101,31 +96,31 @@ const DaoMembers: React.FC = () => {
       </div>
 
       <div>
-        <h3 className='text-3xl mb-4 font-semibold'>Sources</h3>
-        <ul className='text-base text-slate-600 mb-12 list-disc px-4'>
+        <h3 className="text-3xl mb-4 font-semibold">Sources</h3>
+        <ul className="text-base text-slate-600 mb-12 list-disc px-4">
           <li>
             <a
-              target='_blank'
-              className='text-blue-500 active:text-blue-700 hover:text-blue-600'
-              href='https://twitter.com/ZecHub/status/1569827000218537984'
+              target="_blank"
+              className="text-blue-500 active:text-blue-700 hover:text-blue-600"
+              href="https://twitter.com/ZecHub/status/1569827000218537984"
             >
               https://twitter.com/ZecHub/status/1569827000218537984?s=20&t=v6h3n3P7o7LMbnAG-O8Kug
             </a>
           </li>
           <li>
             <a
-              target='_blank'
-              className='text-blue-500 active:text-blue-700 hover:text-blue-600'
-              href='https://forum.zcashcommunity.com/t/zechub-rfi/42778/17'
+              target="_blank"
+              className="text-blue-500 active:text-blue-700 hover:text-blue-600"
+              href="https://forum.zcashcommunity.com/t/zechub-rfi/42778/17"
             >
               https://forum.zcashcommunity.com/t/zechub-rfi/42778/17
             </a>
           </li>
           <li>
             <a
-              target='_blank'
-              className='text-blue-500 active:text-blue-700 hover:text-blue-600'
-              href='https://www.forbes.com/sites/cathyhackl/2021/06/01/what-are-daos-and-why-you-should-pay-attention/?sh=3d34fb2c7305'
+              target="_blank"
+              className="text-blue-500 active:text-blue-700 hover:text-blue-600"
+              href="https://www.forbes.com/sites/cathyhackl/2021/06/01/what-are-daos-and-why-you-should-pay-attention/?sh=3d34fb2c7305"
             >
               https://www.forbes.com/sites/cathyhackl/2021/06/01/what-are-daos-and-why-you-should-pay-attention/?sh=3d34fb2c7305
             </a>
