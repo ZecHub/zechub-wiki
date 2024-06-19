2ee80f80ff736a738d97961fb3104c31a9fe6fc7
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-06-19 19:11:08 +0100

    Update ShieldedPoolDashboard.tsx

--- src/app/dashboard/ShieldedPoolDashboard.tsx
+++ src/app/dashboard/ShieldedPoolDashboard.tsx
@@ -80,14 +80,17 @@ const ShieldedPoolDashboard = () => {
 
   useEffect(() => {
     getBlockchainData().then((data) => setBlockchainInfo(data));
+    
     getSupplyData(sproutUrl).then((data) => {
       setSproutSupply(data[data.length - 1]);
       if (selectedPool === "sprout") setLastUpdated(data[data.length - 1].timestamp);
     });
+    
     getSupplyData(saplingUrl).then((data) => {
       setSaplingSupply(data[data.length - 1]);
       if (selectedPool === "sapling") setLastUpdated(data[data.length - 1].timestamp);
     });
+    
     getSupplyData(orchardUrl).then((data) => {
       setOrchardSupply(data[data.length - 1]);
       if (selectedPool === "orchard") setLastUpdated(data[data.length - 1].timestamp);
@@ -137,11 +140,15 @@ const ShieldedPoolDashboard = () => {
   return (
     <div>
       <h2 className="font-bold mt-8 mb-4">Shielded Supply Chart (ZEC)</h2>
-      <div className="border p-3 rounded-lg relative">
-        <ShieldedPoolChart dataUrl={getDataUrl()} color={getDataColor()} />
-        <span className="absolute bottom-0 right-0 p-2 text-sm text-gray-500">
-          Last updated: {lastUpdated ? new Date(lastUpdated).toLocaleString() : "Loading..."}
-        </span>
+      <div className="border p-3 rounded-lg">
+        <div className="relative">
+          <ShieldedPoolChart dataUrl={getDataUrl()} color={getDataColor()} />
+        </div>
+        <div className="text-right mt-1">
+          <span className="text-sm text-gray-500">
+            Last updated: {lastUpdated ? new Date(lastUpdated).toLocaleString() : "Loading..."}
+          </span>
+        </div>
       </div>
       <div className="mt-8 flex flex-col items-center">
         <div className="flex justify-center space-x-4">
