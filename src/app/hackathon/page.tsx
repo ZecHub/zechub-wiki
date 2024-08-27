7c3458a4e401111717c118c37a06db24b5fa8dc1
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-08-27 17:24:24 +0100

    Update page.tsx

--- src/app/hackathon/page.tsx
+++ src/app/hackathon/page.tsx
@@ -17,7 +17,7 @@ const Hackathon: React.FC = () => {
     <div
       className="min-h-screen bg-cover bg-center p-8"
       style={{
-        backgroundImage: 'url(https://i.ibb.co/BCSVPf3/zechub-hackathon-6.png)',
+        backgroundImage: 'url(https://i.ibb.co/6W5z4m1/zechub-hackathon-3.png)',
       }}
     >
       <h1 className="text-3xl font-bold text-center mb-4">Zechub Hackathon</h1>
