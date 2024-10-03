b17e5b7b5ef293947a62e488946ffb4a3525bead
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-10-03 18:54:29 +0100

    Update Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,31 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import styles from './Gallery.module.css';
+import styles from '../../components/Gallery.module.css';
 
-const Gallery: React.FC = () => {
+const GalleryPage: React.FC = () => {
   const [images, setImages] = useState<string[]>([]);
 
   useEffect(() => {
-    const importAll = (r: any): string[] => r.keys().map(r);
+    if (typeof window !== "undefined") {
+      const importAll = (r: any): string[] => r.keys().map(r);
 
-    try {
-      const images = importAll(
-        (require as any).context('/public/gallery', false, /\.(png|jpe?g|svg|gif)$/)
-      );
-      setImages(images.map((image: any) => image.default.src));
-    } catch (error) {
-      console.error("Error loading images: ", error);
+      try {
+        const images = importAll(
+          (require as any).context('/public/gallery', false, /\.(png|jpe?g|svg|gif)$/)
+        );
+        setImages(images.map((image: any) => image.default.src));
+      } catch (error) {
+        console.error("Error loading images: ", error);
+      }
     }
   }, []);
 
   return (
-    <div className={styles.galleryContainer}>
-      {images.map((src, index) => (
-        <div key={index} className={styles.imageContainer}>
-          <img src={src} alt={`Gallery Image ${index}`} className={styles.image} />
-        </div>
-      ))}
+    <div>
+      <h1>Image Gallery</h1>
+      <div className={styles.galleryContainer}>
+        {images.length === 0 && <p>No images found.</p>}
+        {images.map((src, index) => (
+          <div key={index} className={styles.imageContainer}>
+            <img src={src} alt={`Gallery Image ${index}`} className={styles.image} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default Gallery;
+export default GalleryPage;
