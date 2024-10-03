e50b4a12c57fe72522867b278691e7b620a2727c
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-10-03 18:20:44 +0100

    Create Gallery.tsx

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import styles from './Gallery.module.css'; 
+
+const Gallery = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    
+    const importAll = (r) => r.keys().map(r);
+    const images = importAll(
+      require.context("/public/data/gallery", false, /\.(png|jpe?g|svg|gif)$/)
+    );
+    setImages(images);
+  }, []);
+
+  return (
+    <div className={styles.galleryContainer}>
+      {images.map((image, index) => (
+        <div key={index} className={styles.imageContainer}>
+          <img src={image.default.src} alt={`Gallery Image ${index}`} className={styles.image} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Gallery;
