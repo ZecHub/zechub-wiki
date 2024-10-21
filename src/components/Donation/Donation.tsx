639468c5e78c7eac058cb9dae5ca8fc64a2f67f6
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-10-21 18:54:28 +0100

    Update Donation.tsx

--- src/components/Donation/Donation.tsx
+++ src/components/Donation/Donation.tsx
@@ -12,8 +12,10 @@ const DonationComp = () => {
   const [selectedCurrency, setSelectedCurrency] = useState('zcash'); // Track selected currency
   const [error, setError] = useState<string | null>(null); // Error state for invalid input
 
-  const predefinedAmounts = [0.25, 0.5, 1.0, 2.5, 5];
-  
+  // Predefined amounts for Zcash and Ycash
+  const predefinedZecAmounts = [0.25, 0.5, 1.0, 2.5, 5];
+  const predefinedYecAmounts = [100, 200, 400, 1000];
+
   const zcashAddress = 'zcash:u1rl2zw85dmjc8m4dmqvtstcyvdjn23n0ad53u5533c97affg9jq208du0vf787vfx4vkd6cd0ma4pxkkuc6xe6ue4dlgjvn9dhzacgk9peejwxdn0ksw3v3yf0dy47znruqftfqgf6xpuelle29g2qxquudxsnnen3dvdx8az6w3tggalc4pla3n4jcs8vf4h29ach3zd8enxulush89';
   const ycashAddress = 'ys1t2e77wawylp8zky7wq3gzky2j4w6rpgd8632vmvqqj370thgpls8t973qutj4gn5wsc3qmcy56y';
 
@@ -26,7 +28,7 @@ const DonationComp = () => {
   const handleChangeAmount = (event: ChangeEvent<HTMLInputElement>) => {
     const value = parseFloat(event.target.value);
     if (isNaN(value) || value <= 0) {
-      setError('Please enter a valid amount greater than 0 ZEC');
+      setError('Please enter a valid amount greater than 0');
     } else {
       setDonationAmount(value);
       setMemo(event.target.value); // Synchronize memo with donation amount
@@ -37,7 +39,7 @@ const DonationComp = () => {
   const handleChangeMemo = (event: ChangeEvent<HTMLInputElement>) => {
     const value = parseFloat(event.target.value);
     if (isNaN(value) || value <= 0) {
-      setError('Please enter a valid amount greater than 0 ZEC');
+      setError('Please enter a valid amount greater than 0');
     } else {
       setDonationAmount(value);
       setMemo(event.target.value);
@@ -74,18 +76,38 @@ const DonationComp = () => {
           Copy & Open Wallet
         </button>
 
-        <div className="currency-switch" style={{ marginTop: '10px' }}>
+        <div className="currency-switch" style={{ marginTop: '10px', display: 'flex', justifyContent: 'center' }}>
           <button
             onClick={() => setSelectedCurrency('zcash')}
             className={selectedCurrency === 'zcash' ? 'active' : ''}
-            style={{ borderRadius: '50%', padding: '10px', margin: '5px' }}
+            style={{
+              borderRadius: '50%',
+              padding: '10px',
+              margin: '5px',
+              border: selectedCurrency === 'zcash' ? '3px solid lightblue' : '3px solid transparent',
+              width: '80px',
+              height: '80px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
           >
             <Image src={zcashLogo} alt='Zcash' width={32} height={32} />
           </button>
           <button
             onClick={() => setSelectedCurrency('ycash')}
             className={selectedCurrency === 'ycash' ? 'active' : ''}
-            style={{ borderRadius: '50%', padding: '10px', margin: '5px' }}
+            style={{
+              borderRadius: '50%',
+              padding: '10px',
+              margin: '5px',
+              border: selectedCurrency === 'ycash' ? '3px solid lightblue' : '3px solid transparent',
+              width: '80px',
+              height: '80px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
           >
             <Image src={ycashLogo} alt='Ycash' width={32} height={32} />
           </button>
@@ -103,36 +125,36 @@ const DonationComp = () => {
           </p>
 
           <div className='input-range'>
-            <label>Choose your donation amount (ZEC):</label>
+            <label>Choose your donation amount ({selectedCurrency === 'zcash' ? 'ZEC' : 'YEC'}):</label>
             <input
               type='range'
-              min='0.25'
-              max='5'
-              step='0.05'
-              value={donationAmount <= 5 ? donationAmount : 5} // Sync with slider up to 5 ZEC
+              min={selectedCurrency === 'zcash' ? '0.25' : '100'}
+              max={selectedCurrency === 'zcash' ? '5' : '1000'}
+              step={selectedCurrency === 'zcash' ? '0.05' : '50'}
+              value={donationAmount <= (selectedCurrency === 'zcash' ? 5 : 1000) ? donationAmount : 5} // Sync with slider
               onChange={handleChangeAmount}
               className='slider'
             />
           </div>
 
           <div className='amount'>
-            <p>Amount: {donationAmount} </p>
+            <p>Amount: {donationAmount} {selectedCurrency === 'zcash' ? 'ZEC' : 'YEC'}</p>
           </div>
 
           <div className='amount-buttons'>
-            {predefinedAmounts.map((amount, index) => (
+            {(selectedCurrency === 'zcash' ? predefinedZecAmounts : predefinedYecAmounts).map((amount, index) => (
               <button key={index} onClick={() => handleSelectAmount(amount.toString())}>
-                {amount} ZEC
+                {amount} {selectedCurrency === 'zcash' ? 'ZEC' : 'YEC'}
               </button>
             ))}
           </div>
 
           <div className='input-number'>
-            <label>Enter Amount (ZEC):</label>
+            <label>Enter Amount ({selectedCurrency === 'zcash' ? 'ZEC' : 'YEC'}):</label>
             <input
               type='number'
               value={memo}
-              placeholder='Enter any amount in ZEC'
+              placeholder={`Enter any amount in ${selectedCurrency === 'zcash' ? 'ZEC' : 'YEC'}`}
               onChange={handleChangeMemo}
             />
           </div>
