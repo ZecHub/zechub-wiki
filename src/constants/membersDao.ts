28b6e30521638f0b0de0b73970c2b1b0289f567e
squirrel <47972284+zksquirrel@users.noreply.github.com>
2024-11-05 17:02:09 +0000

    Update membersDao.ts

--- src/constants/membersDao.ts
+++ src/constants/membersDao.ts
@@ -170,7 +170,7 @@ export const daoMembers = [
         and development of the Zcash ecosystem.`,
         linkName: 'Twitter',
         urlLink: 'https://twitter.com/zksquirrel',
-        zcashAddress: 'zs1huxhjdm23gnm67g9neyemvn65s5vrkcv496ngwumk0pkw7tlu0lsgf5smrvncc8f0fwxquz6s0j'
+        zcashAddress: 'u1n6sscrlxhz8a9wlvfa076rux7q00lff48jt62kje09ds5ntynlp2hcrsf3emtprts3z59yt99cvzwvnz7lvzgrpdxqrj3kxfx98y2pt46qry87rqcfuj02x3xsj0jqqnehhzd8hy090tntqwsx8ncatsckzmnw43yqqntuv668av4vhqf2p6payrz94cstm2v465f4nllmpawp5jcat'
     },
 
 
