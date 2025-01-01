13a416e20e5af78e015036ce740e58ffae49a5c4
squirrel <47972284+zksquirrel@users.noreply.github.com>
2025-01-01 14:12:14 +0000

    Update Grid.tsx

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -14,8 +14,8 @@ const projects = [
   },
   {
     title: "Zavax Redbridge",
-    description: "Description for Zavax Redbridge... ",
-    link: "#",
+    description: "The Zcash-Avalanche Red-Bridge is a decentralized bridge that enables interoperability between the Zcash (ZEC) and Avalanche (AVAX) blockchains,...",
+    link: "https://zechub.wiki/guides/zcash-avalanche-redbridge#content",
     imageUrl:
       "https://images.ctfassets.net/jg6lo9a2ukvr/27nB85zQBKXGOyMzsGpH6r/0fc7d99bc7a53a90002614480e16f4c9/AVAX-TokenLaunch-Blog-Header.png?fm=webp",
   },
@@ -32,6 +32,13 @@ const projects = [
       "Zcash Shielded Assets (ZSA) are a proposed improvement to the the Zcash protocol that would enable the creation,... ",
     link: "https://zechub.wiki/zcash-tech/zcash-shielded-assets#content",
     imageUrl: "https://i.ibb.co/0VfMFB5/image-2023-11-18-160742427.png",
+  },
+  {
+    title: "Hybrid Proof of Stake (Crosslink)",
+    description:
+      "The Crosslink protocol is a proposed design for Zcash hybrid Proof-of-Work/Proof-of-Stake (PoW/PoS) stage,...",
+    link: "https://zechub.wiki/zcash-tech/crosslink_protocol#content",
+    imageUrl: "https://i.ibb.co/6HZpChV/image-2025-01-01-141008115.png",
   },
 ];
 
