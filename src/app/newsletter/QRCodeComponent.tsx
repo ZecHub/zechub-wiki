2db15328a7d3739bfb4f621b19a5b43b240552a3
squirrel <47972284+zksquirrel@users.noreply.github.com>
2025-01-10 14:40:06 +0000

    Update QRCodeComponent.tsx

--- src/app/newsletter/QRCodeComponent.tsx
+++ src/app/newsletter/QRCodeComponent.tsx
@@ -2,20 +2,12 @@ import React from "react";
 import QRCode from "qrcode.react";
 
 interface QRCodeComponentProps {
-  prefix: string;
   memo: string;
-  encrypt: boolean;
-  fec: boolean;
   className?: string;
 }
 
-const QRCodeComponent: React.FC<QRCodeComponentProps> = ({
-  prefix,
-  memo,
-  encrypt,
-  fec,
-  className,
-}) => {
+const QRCodeComponent: React.FC<QRCodeComponentProps> = ({ memo, className }) => {
+  // Base64 URL Encoding function
   const base64UrlEncode = (input: string): string => {
     const base64Encoded = btoa(unescape(encodeURIComponent(input)));
     return base64Encoded
@@ -24,21 +16,15 @@ const QRCodeComponent: React.FC<QRCodeComponentProps> = ({
       .replace(/=+$/, "");
   };
 
-  const generateMessageLink = () => {
+  // Generate Zcash Payment URI
+  const generateMessageLink = (): string => {
     const zcashAddress =
       "zcash:u1n6sscrlxhz8a9wlvfa076rux7q00lff48jt62kje09ds5ntynlp2hcrsf3emtprts3z59yt99cvzwvnz7lvzgrpdxqrj3kxfx98y2pt46qry87rqcfuj02x3xsj0jqqnehhzd8hy090tntqwsx8ncatsckzmnw43yqqntuv668av4vhqf2p6payrz94cstm2v465f4nllmpawp5jcat";
-    let baseAmount = 0.05; // Example base amount
-    if (encrypt) baseAmount += 0.05;
-    if (fec) baseAmount += 0.05;
+    const baseAmount = 0.05; // Example base amount in ZEC
 
-    const formattedAmount = baseAmount.toFixed(2);
-    const combinedInfo = `${prefix} ${encrypt ? "e1" : "e0"} ${
-      fec ? "f1" : "f0"
-    }`.trim();
-    const fullMemo = `${combinedInfo} | ${memo}`;
-    const encodedMemo = base64UrlEncode(fullMemo);
+    const encodedMemo = base64UrlEncode(memo);
 
-    return `${zcashAddress}?amount=${formattedAmount}&memo=${encodedMemo}`;
+    return `${zcashAddress}?amount=${baseAmount}&memo=${encodedMemo}`;
   };
 
   return (
