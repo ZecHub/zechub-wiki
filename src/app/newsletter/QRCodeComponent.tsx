a783b090723850e2ea6ae9e60574dd0e6a7011b1
squirrel <47972284+zksquirrel@users.noreply.github.com>
2025-02-05 17:14:18 +0000

    Update QRCodeComponent.tsx

--- src/app/newsletter/QRCodeComponent.tsx
+++ src/app/newsletter/QRCodeComponent.tsx
@@ -3,10 +3,12 @@ import QRCode from "qrcode.react";
 
 interface QRCodeComponentProps {
   memo: string;
+  address: string;
+  amount: number;
   className?: string;
 }
 
-const QRCodeComponent: React.FC<QRCodeComponentProps> = ({ memo, className }) => {
+const QRCodeComponent: React.FC<QRCodeComponentProps> = ({ memo, address, amount, className }) => {
   // Base64 URL Encoding function
   const base64UrlEncode = (input: string): string => {
     const base64Encoded = btoa(unescape(encodeURIComponent(input)));
@@ -16,15 +18,10 @@ const QRCodeComponent: React.FC<QRCodeComponentProps> = ({ memo, className }) =>
       .replace(/=+$/, "");
   };
 
-  // Generate Zcash Payment URI
+  // Generate Zcash Payment URI using provided props
   const generateMessageLink = (): string => {
-    const zcashAddress =
-      "zcash:u1hu6m28c4xkufyftytug30wvaknajs2dqdy0nvh4yz9xq854p3chxv8per9rdsdyt7ycaeddp7u5xgmn3ghvkh4sku785fy4m5vgqsv0r";
-    const baseAmount = 0.05; // Example base amount in ZEC
-
     const encodedMemo = base64UrlEncode(memo);
-
-    return `${zcashAddress}?amount=${baseAmount}&memo=${encodedMemo}`;
+    return `zcash:${address}?amount=${amount}&memo=${encodedMemo}`;
   };
 
   return (
