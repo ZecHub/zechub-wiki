35610d33c771933ec5ad12f5b315081e8e0fc709
MANUEL RIOS <m.rios56@hotmail.com>
2025-02-09 17:43:38 +0000

    fixing mdx

--- src/components/MdxComponents/ConfigComponent.tsx
+++ src/components/MdxComponents/ConfigComponent.tsx
@@ -1,12 +1,12 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { HTMLProps } from "react";
+import { HTMLProps, JSX } from "react";
 import { transformGithubFilePathToWikiLink } from "@/lib/helpers";
 import type { MDXComponents } from 'mdx/types'
 
 const MdxComponents: MDXComponents = {
-  img: (props: HTMLProps<HTMLImageElement>) => {
+  img: (props: HTMLProps<HTMLImageElement>): JSX.Element => {
     if (props.src?.startsWith('/')) props.src = 'https://github.com/ZecHub/zechub/tree/main' + props.src
     return (
       <Image
@@ -20,7 +20,7 @@ const MdxComponents: MDXComponents = {
     )
   },
 
-  a: (props: HTMLProps<HTMLHyperlinkElementUtils>) => {
+  a: (props: HTMLProps<HTMLHyperlinkElementUtils>): JSX.Element  => {
 
     return (
 
@@ -30,105 +30,105 @@ const MdxComponents: MDXComponents = {
     )
   },
 
-  pre: (props: HTMLProps<HTMLPreElement>) => (
+  pre: (props: HTMLProps<HTMLPreElement>): JSX.Element  => (
     <pre {...props} >{props.children}</pre>
   ),
 
-  em: (props: HTMLProps<HTMLSpanElement>) => (
+  em: (props: HTMLProps<HTMLSpanElement>): JSX.Element  => (
     <em {...props}>{props.children}</em>
   ),
 
-  blockquote: (props: HTMLProps<HTMLQuoteElement>) => (
+  blockquote: (props: HTMLProps<HTMLQuoteElement>): JSX.Element  => (
     <blockquote className="text-base text-justify my-2" {...props}>
       {props.children}
     </blockquote>
   ),
 
-  ul: (props: HTMLProps<HTMLUListElement>) => (
+  ul: (props: HTMLProps<HTMLUListElement>): JSX.Element  => (
     <ul {...props} className="list-outside">
       {props.children}
     </ul>
   ),
 
-  li: (props: HTMLProps<HTMLLIElement>) => (
+  li: (props: HTMLProps<HTMLLIElement>): JSX.Element  => (
     <li {...props}>{props.children}</li>
   ),
 
-  h1: (props: HTMLProps<HTMLHeadingElement>) => (
+  h1: (props: HTMLProps<HTMLHeadingElement>): JSX.Element  => (
     <h1 className="text-5xl font-bold my-4" {...props}>
       {props.children}
     </h1>
   ),
 
-  h2: (props: HTMLProps<HTMLHeadingElement>) => (
+  h2: (props: HTMLProps<HTMLHeadingElement>): JSX.Element  => (
     <h2 className="text-4xl font-bold my-4" {...props}>
       {props.children}
     </h2>
   ),
 
-  h3: (props: HTMLProps<HTMLHeadingElement>) => (
+  h3: (props: HTMLProps<HTMLHeadingElement>): JSX.Element  => (
     <h3 className="text-3xl font-bold my-4" {...props}>
       {props.children}
     </h3>
   ),
 
-  h4: (props: HTMLProps<HTMLHeadingElement>) => (
+  h4: (props: HTMLProps<HTMLHeadingElement>): JSX.Element  => (
     <h4 className="text-2xl text-lg font-bold my-4" {...props}>
       {props.children}
     </h4>
   ),
 
-  h5: (props: HTMLProps<HTMLHeadingElement>) => (
+  h5: (props: HTMLProps<HTMLHeadingElement>): JSX.Element  => (
     <h5 className="text-xl font-bold my-4" {...props}>
       {props.children}
     </h5>
   ),
 
-  h6: (props: HTMLProps<HTMLHeadingElement>) => (
+  h6: (props: HTMLProps<HTMLHeadingElement>): JSX.Element  => (
     <h6 className="text-lg font-bold my-4" {...props}>
       {props.children}
     </h6>
   ),
 
-  p: (props: HTMLProps<HTMLParagraphElement>) => (
+  p: (props: HTMLProps<HTMLParagraphElement>): JSX.Element  => (
     <p className="text-base text-justify my-4" {...props}>
       {props.children}
     </p>
   ),
-  strong: (props: HTMLProps<HTMLHtmlElement>) => (
+  strong: (props: HTMLProps<HTMLHtmlElement>): JSX.Element  => (
     <strong className="font-bold">{props.children}</strong>
   ),
 
-  table: (props: HTMLProps<HTMLTableElement>) => (
+  table: (props: HTMLProps<HTMLTableElement>): JSX.Element  => (
     <table className="table-fixed">
       {props.children}
     </table>
   ),
 
-  thead: (props: HTMLProps<HTMLTableSectionElement>) => (
+  thead: (props: HTMLProps<HTMLTableSectionElement>): JSX.Element  => (
     <thead >
       {props.children}
     </thead>
   ),
 
-  tr: (props: HTMLProps<HTMLTableRowElement>) => (
+  tr: (props: HTMLProps<HTMLTableRowElement>): JSX.Element  => (
     <tr className="p-3 py-3 border-2">
       {props.children}
     </tr>
   ),
 
-  tbody: (props: HTMLProps<HTMLTableSectionElement>) => (
+  tbody: (props: HTMLProps<HTMLTableSectionElement>): JSX.Element  => (
     <tbody className="p-3 ">
       {props.children}
     </tbody>
   ),
 
-  th: (props: HTMLProps<HTMLTableCellElement>) => (
+  th: (props: HTMLProps<HTMLTableCellElement>): JSX.Element  => (
     <th>
       {props.children}
     </th>
   ),
-  td: (props: HTMLProps<HTMLTableCellElement>) => (
+  td: (props: HTMLProps<HTMLTableCellElement>): JSX.Element  => (
     <td className="p-3">
       {props.children}
     </td>
