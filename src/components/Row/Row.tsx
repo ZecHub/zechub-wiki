ce3362f0253945a8ba4ad966dd27e6d0b2db55d6
Ekene Emmanuel <kenzynuel@gmail,com>
2025-02-11 03:06:57 +0100

    Added New Brand Card

--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -1,6 +1,7 @@
 // components/ProjectRow.tsx
 import React from "react";
 import BrandCards from "../BrandCards/BrandCards";
+import zcashNigeria from "../../assets/brand/ZcashNigeria/JPG/ZcashNigeria_Black.jpg";
 import zcashEspanol from "../../assets/brand/ZcashEspanol/JPG/ZcashEspanol.jpg";
 import ecc from "../../assets/brand/ECC/JPG/ECCBlack.jpg";
 import wallet from "../../assets/brand/Wallets/Zashi/JPG/ZashiIconBlack.jpg";
@@ -49,8 +50,15 @@ const projects = [
   {
     title: "Zcash En Español",
     description: "Zcash en Español provides Spanish resources, news, guides, and community support for Zcash users.",
-    link: "downloads/brand/ZcashEspanol.zip",
+    link: "downloads/brand/ZcashNigeria.zip",
     imageUrl: zcashEspanol,
+    size: 240,
+  },
+  {
+    title: "Zcash Nigeria",
+    description: "Zcash Nigeria promotes financial privacy by educating and onboarding Nigerians to Zcash",
+    link: "downloads/brand/ZcashEspanol.zip",
+    imageUrl: zcashNigeria,
     size: 240,
   },
 ];
