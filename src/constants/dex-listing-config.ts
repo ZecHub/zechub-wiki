ce5ac58d90908f77ea9360c4aeea924bfd394c50
0xmtg <108434941+Manuelthegoat@users.noreply.github.com>
2025-02-21 01:02:24 +0100

    Add files via upload

---
+++ src/constants/dex-listing-config.ts
@@ -0,0 +1,94 @@
+export const dexListingConfig = [
+  
+    {
+    title: 'LetsExchange',
+    description:
+      'A crypto exchange hub with 4,500+ cryptocurrencies, deep liquidity, cross-chain swaps, on- & off-ramp, DEX, swift and secure transactions.',
+    url: 'https://letsexchange.io/',
+    image: 'https://i.ibb.co/JxzSyx6/1500x500-1.jpg',
+  },
+  {
+    title: 'Near-intents',
+    description:
+      'Fast exchange with the support of NEAR. Make deposits, sell, swap including popular TRUMP, MELANIA, BERA and other memes',
+    url: 'https://app.near-intents.org/',
+    image: '/nearintents.png',
+  },
+  {
+    title: 'Stealth EX',
+    description:
+      'StealthEX is an instant cryptocurrency exchange for limitless swaps whose service is free from registration and does not store user’s funds on the platform. StealthEX is the best place to buy cryptocurrency.',
+    url: 'https://stealthex.io/',
+    image: '/stealth_ex.png',
+  },
+  {
+    title: 'Changelly',
+    description:
+      'Changelly is an instant-execution crypto coin exchange that is gloriously known for its industry-low fees of up to 0.25% on crypto exchanges. It also meets the definition of the most versatile crypto exchange.',
+    url: 'https://changelly.com/',
+    image: '/changelly.png',
+  },
+  {
+    title: 'flyp.me',
+    description:
+      'The Flyp.me accountless exchanger and the FYP Token were born to protect privacy, promote decentralisation, support grassroot ownership and foster a community believing in consensus.',
+    url: 'https://flyp.me/en/#/',
+    image: '/flyp.me.png',
+  },
+  {
+    title: 'Exolix',
+    description:
+      'Exchange 1311+ crypto instantly, with the best rate and without registration',
+    url: 'https://exolix.com',
+    image: '/exolix.png',
+  },
+  {
+    title: 'Trocodor',
+    description:
+      'Trade Cryptocurrency Anonymously. Fast. Safe. No Bureaucracy. Prepaid Debit Cards available.',
+    url: 'https://trocador.app/',
+    image: '/Trocodor.png',
+  },
+  {
+    title: 'Bison Wallet',
+    description:
+      'Trade crypto peer to peer. No trading fees. No KYC. Decentralised Exchange built by Decred Project.',
+    url: 'https://dex.decred.org/',
+    image: '/DCRDEX.jpg',
+  },
+  {
+    title: 'Firo DEX',
+    description:
+      'Trustless decentralized swaps using atomic swaps with FiroDEX!.',
+    url: 'https://github.com/firoorg/FiroDEX-Desktop',
+    image: 'https://firo.org/blog/assets/firodex-beta-release.png',
+  },
+  {
+    title: 'Changenow',
+    description: 
+      'Limitless WEB 3 Crypto Exchange – Simple, Fast, Custody-free.',
+    url: 'https://changenow.io/?from=usdterc20&to=zec',
+    image: '/Changenow.png',
+  },
+  {
+    title: 'Fixed Float',
+    description:
+      'Instant, fully automatic cryptocurrency exchange with Lightning Network.',
+    url: 'https://ff.io',
+    image: '/FixedFloat.jpg',
+  },
+  {
+    title: 'Xchange',
+    description:
+      'Anonymous Cryptocurrency Exchange, cheap, reliable, AlwaysOnTime and without any registration. Dedicated Tor mirror available.',
+    url: 'https://xchange.me/',
+    image: '/xchange.png',
+  },
+  {
+    title: 'Bitcoin VN',
+    description:
+      'Instant Bitcoin & Cryptocurrency Exchange in Vietnam. Buy, sell or swap between 80 assets including VND, BTC, XMR, USDT, ETH, BCH, SOL',
+    url: 'https://bitcoinvn.io/?deposit=xmr&settle=zec/',
+    image: '/Bitcoinvn.png',
+  },
+];
