78403a45566be998f9abf53343efb2ec085229e9
amochuko <7204868+amochuko@users.noreply.github.com>
2025-07-20 16:03:22 +0100

    update: add shielded pool

--- src/components/ShieldedPool/index.tsx
+++ src/components/ShieldedPool/index.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
+import Dashboard from "../Charts";
 
-const ShieldedPoolDashboard = dynamic(
-  () => import("./ShieldedPoolDashboard"),
-  { 
-    ssr: false,
-    loading: () => <span className="text-center text-3xl">Loading...</span>
-  }
-);
+const ShieldedPoolDashboard = dynamic(() => import("./ShieldedPoolDashboard"), {
+  ssr: false,
+  loading: () => <span className="text-center text-3xl">Loading...</span>,
+});
 
 export default function ClientDashboard() {
-  return <ShieldedPoolDashboard />;
+  // return <ShieldedPoolDashboard />;
+  return <Dashboard />;
 }
