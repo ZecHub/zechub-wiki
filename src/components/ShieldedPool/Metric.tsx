c9d10935037779f20a97d18dfb5e7aeb83a80399
amochuko <7204868+amochuko@users.noreply.github.com>
2025-07-21 00:29:31 +0100

    fix: refactor

--- src/components/ShieldedPool/Metric.tsx
+++ src/components/ShieldedPool/Metric.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { MetricCard } from "../ZcashMetrics/MetricCard";
 
 interface CoinData {
   usd?: number;
@@ -96,18 +97,22 @@ const CryptoMetrics = ({ selectedCoin }: { selectedCoin: string }) => {
         setCoinData(coinInfo);
         console.log(name.toLowerCase(), "coinInfo", data);
 
+        console.log({ coinInfo });
+
         // Mock blockchain data (replace with actual API calls)
         setBlockchainInfo({
-          market_cap_usd: coinInfo.usd_market_cap || 0,
-          market_price_usd: coinInfo.usd || 0,
-          market_price_btc: coinInfo.btc || 0,
+          market_cap_usd: coinInfo?.usd_market_cap || 0,
+          market_price_usd: coinInfo?.usd || 0,
+          market_price_btc: coinInfo?.btc || 0,
           blocks: Math.floor(Math.random() * 2000000),
-          transactions_24h: coinInfo.usd_24h_vol || 0,
+          transactions_24h: coinInfo?.usd_24h_vol || 0,
         });
 
         setCirculation(
           Math.floor(
-            coinInfo.usd_market_cap ? coinInfo.usd_market_cap / coinInfo.usd : 0
+            coinInfo?.usd_market_cap
+              ? coinInfo?.usd_market_cap / coinInfo?.usd
+              : 0
           )
         );
       } catch (err) {
@@ -121,41 +126,82 @@ const CryptoMetrics = ({ selectedCoin }: { selectedCoin: string }) => {
     fetchCoinData();
   }, [selectedCoin]);
 
+  const metricsObj = [
+    {
+      label: "Market Cap",
+      value: `$${blockchainInfo?.market_cap_usd.toLocaleString() ?? "N/A"}`,
+    },
+    {
+      label: "Circulation",
+      value: `${circulation?.toLocaleString() ?? "N/A"} ${selectedCoin}`,
+    },
+    {
+      label: "Market Price (USD)",
+      value: `$${blockchainInfo?.market_price_usd.toFixed(2) ?? "N/A"}`,
+    },
+    {
+      label: "Market Price (BTC)",
+      value: Number(blockchainInfo?.market_price_btc).toFixed(8),
+      // value: isMobile
+      //   ? Number(blockchainInfo?.market_price_btc).toFixed(4) ?? "N/A"
+      //   : Number(blockchainInfo?.market_price_btc).toFixed(8) ?? "N/A",
+    },
+    {
+      label: "Blocks",
+      value: blockchainInfo?.blocks.toLocaleString() ?? "N/A",
+    },
+    {
+      label: "24h Transactions",
+      value: blockchainInfo?.transactions_24h.toLocaleString() ?? "N/A",
+    },
+  ];
 
   return (
-    <div className="flex flex-wrap gap-8 justify-center items-center mt-8">
-      <div className="border p-4 rounded-md text-center min-w-[200px]">
-        <h3 className="font-bold text-lg">Market Cap</h3>
-        <p>${blockchainInfo?.market_cap_usd?.toLocaleString()}</p>
-      </div>
+    // <div className="flex flex-wrap gap-8 justify-center items-center mt-8">
+    //   <div className="border p-4 rounded-md text-center min-w-[200px]">
+    //     <h3 className="font-bold text-lg">Market Cap</h3>
+    //     <p>${blockchainInfo?.market_cap_usd?.toLocaleString()}</p>
+    //   </div>
 
-      <div className="border p-4 rounded-md text-center min-w-[200px]">
-        <h3 className="font-bold text-lg">{selectedCoin} in Circulation</h3>
-        <p>
-          {circulation?.toLocaleString()} {selectedCoin}
-        </p>
-      </div>
+    //   <div className="border p-4 rounded-md text-center min-w-[200px]">
+    //     <h3 className="font-bold text-lg">{selectedCoin} in Circulation</h3>
+    //     <p>
+    //       {circulation?.toLocaleString()} {selectedCoin}
+    //     </p>
+    //   </div>
 
-      <div className="border p-4 rounded-md text-center min-w-[200px]">
-        <h3 className="font-bold text-lg">Market Price (USD)</h3>
-        <p>${blockchainInfo?.market_price_usd?.toFixed(2)}</p>
-      </div>
+    //   <div className="border p-4 rounded-md text-center min-w-[200px]">
+    //     <h3 className="font-bold text-lg">Market Price (USD)</h3>
+    //     <p>${blockchainInfo?.market_price_usd?.toFixed(2)}</p>
+    //   </div>
 
-      <div className="border p-4 rounded-md text-center min-w-[200px]">
-        <h3 className="font-bold text-lg">Market Price (BTC)</h3>
-        <p>{blockchainInfo?.market_price_btc?.toFixed(8)}</p>
-      </div>
+    //   <div className="border p-4 rounded-md text-center min-w-[200px]">
+    //     <h3 className="font-bold text-lg">Market Price (BTC)</h3>
+    //     <p>{blockchainInfo?.market_price_btc?.toFixed(8)}</p>
+    //   </div>
 
-      {/* {blockchainInfo?.blocks && (
-        <div className="border p-4 rounded-md text-center min-w-[200px]">
-          <h3 className="font-bold text-lg">Blocks</h3>
-          <p>{blockchainInfo.blocks.toLocaleString()}</p>
-        </div>
-      )} */}
+    //   {/* {blockchainInfo?.blocks && (
+    //     <div className="border p-4 rounded-md text-center min-w-[200px]">
+    //       <h3 className="font-bold text-lg">Blocks</h3>
+    //       <p>{blockchainInfo.blocks.toLocaleString()}</p>
+    //     </div>
+    //   )} */}
 
-      <div className="border p-4 rounded-md text-center min-w-[200px]">
-        <h3 className="font-bold text-lg">24h Transactions</h3>
-        <p>{blockchainInfo?.transactions_24h?.toLocaleString()}</p>
+    //   <div className="border p-4 rounded-md text-center min-w-[200px]">
+    //     <h3 className="font-bold text-lg">24h Transactions</h3>
+    //     <p>{blockchainInfo?.transactions_24h?.toLocaleString()}</p>
+    //   </div>
+    // </div>
+
+    <div className="my-12">
+      <h2 className="font-bold text-xl text-slate-700 dark:text-slate-100">
+        {selectedCoin} Metrics
+      </h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">
+        {metricsObj.map(({ label, value }) => (
+          <MetricCard label={label} value={value} key={label} />
+        ))}
       </div>
     </div>
   );
