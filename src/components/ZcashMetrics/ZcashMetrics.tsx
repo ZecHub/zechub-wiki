9a5b23e8863c2417036ac7896f3290fe7d9fca54
amochuko <7204868+amochuko@users.noreply.github.com>
2025-07-20 16:14:52 +0100

    update: add metric comp

---
+++ src/components/ZcashMetrics/ZcashMetrics.tsx
@@ -0,0 +1,66 @@
+import { useInMobile } from "@/hooks/useIsMobile";
+import { BlockchainInfo } from "@/lib/chart/types";
+import { MetricCard } from "./MetricCard";
+
+interface ZcashStatisticsPorps {
+  blockchainInfo: BlockchainInfo;
+  circulation: number;
+  shieldedTxCount: any[];
+}
+
+/* Metrics */
+export function ZcashMetrics(props: ZcashStatisticsPorps) {
+  const isMobile = useInMobile();
+
+  const metricsObj = [
+    {
+      label: "Market Cap",
+      value: `$${props.blockchainInfo.market_cap_usd.toLocaleString()}`,
+    },
+    {
+      label: "Circulation",
+      value: `${props.circulation?.toLocaleString() ?? "N/A"} ZEC`,
+    },
+    {
+      label: "Market Price (USD)",
+      value: `$${props.blockchainInfo.market_price_usd.toFixed(2)}`,
+    },
+    {
+      label: "Market Price (BTC)",
+      value: isMobile
+        ? Number(props.blockchainInfo.market_price_btc).toFixed(4)
+        : Number(props.blockchainInfo.market_price_btc).toFixed(8),
+    },
+    {
+      label: "Blocks",
+      value: props.blockchainInfo.blocks.toLocaleString(),
+    },
+    {
+      label: "24h Transactions",
+      value: props.blockchainInfo.transactions_24h.toLocaleString(),
+    },
+    {
+      label: "Shielded TX (24h)",
+      value: props.shieldedTxCount?.length
+        ? `Sapling: ${props.shieldedTxCount
+            .at(-1)!
+            .sapling.toLocaleString()} | Orchard: ${props.shieldedTxCount
+            .at(-1)!
+            .orchard.toLocaleString()}`
+        : "N/A",
+    },
+  ];
+
+  return (
+    <div className="my-12">
+      <h1 className="font-bold text-xl text-slate-700 dark:text-slate-100">
+        Zcash Metrics
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">
+        {metricsObj.map(({ label, value }) => (
+          <MetricCard label={label} value={value} key={label} />
+        ))}
+      </div>
+    </div>
+  );
+}
