f3d37531709d5556b8831284a0dfc4ca457deb37
amochuko <7204868+amochuko@users.noreply.github.com>
2025-07-22 12:30:27 +0100

    fix: refactor file

--- src/components/Charts/Zcash/ZcashChart.tsx
+++ src/components/Charts/Zcash/ZcashChart.tsx
@@ -9,7 +9,6 @@ import {
   getBlockchainData,
   getBlockchainInfo,
   getDifficultyData,
-  getIssuanceData,
   getLockboxData,
   getNetInOutflowData,
   getNodeCountData,
@@ -26,31 +25,15 @@ import {
   ShieldedTxCount,
   SupplyData,
 } from "@/lib/chart/types";
-import { Spinner } from "flowbite-react";
 import { RefObject, useEffect, useState } from "react";
-import {
-  Area,
-  AreaChart,
-  CartesianGrid,
-  Legend,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts";
 import { ErrorBoundary } from "../../ErrorBoundary/ErrorBoundary";
 import SupplyDataLastUpdated from "../../LastUpdated";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../../ui/shadcn/select";
 import { ZcashMetrics } from "../../ZcashMetrics/ZcashMetrics";
+import DifficultyChart from "./DifficultyChart";
 import IssuanceChart from "./IssuanceChart";
+import LockboxChart from "./LockboxChart";
+import NetInflowsOutflowsChart from "./NetInflowsOutflowsChart";
+import ShieldedSupplyChart from "./ShieldedSupplyChart";
 
 type ZcashChartProps = {
   lastUpdated: Date;
@@ -153,7 +136,7 @@ function ZcashChart(props: ZcashChartProps) {
           lockboxData,
           netInOutflow,
           difficultyData,
-          issuanceData,
+          // issuanceData,
         ] = await Promise.all([
           // fetchShieldedSupplyData(DATA_URL.shieldedTxCountUrl, controller.signal),
           getBlockchainData(DATA_URL.blockchairUrl, controller.signal),
@@ -170,7 +153,7 @@ function ZcashChart(props: ZcashChartProps) {
             controller.signal
           ),
           getDifficultyData(DATA_URL.difficultyUrl, controller.signal),
-          getIssuanceData(DATA_URL.issuanceUrl, controller.signal),
+          // getIssuanceData(DATA_URL.issuanceUrl, controller.signal),
         ]);
 
         if (netInOutflow) {
@@ -299,6 +282,7 @@ function ZcashChart(props: ZcashChartProps) {
     })),
   ].sort((a, b) => new Date(a.close).getTime() - new Date(b.close).getTime());
 
+  const tabLabels = ["Supply", "Difficulty", "Issuance", "Lockbox", "Flows"];
   return (
     <ErrorBoundary fallback={`Failed to load Namada's chart`}>
       <div className="space-y-6">
@@ -319,41 +303,16 @@ function ZcashChart(props: ZcashChartProps) {
               {({ activeTab, setActiveTab }: any) => (
                 <>
                   <TabsList>
-                    <TabsTrigger
-                      value="supply"
-                      activeTab={activeTab}
-                      setActiveTab={setActiveTab}
-                    >
-                      Supply
-                    </TabsTrigger>
-                    <TabsTrigger
-                      value="difficulty"
-                      activeTab={activeTab}
-                      setActiveTab={setActiveTab}
-                    >
-                      Difficulty
-                    </TabsTrigger>
-                    <TabsTrigger
-                      value="issuance"
-                      activeTab={activeTab}
-                      setActiveTab={setActiveTab}
-                    >
-                      Issuance
-                    </TabsTrigger>
-                    <TabsTrigger
-                      value="lockbox"
-                      activeTab={activeTab}
-                      setActiveTab={setActiveTab}
-                    >
-                      Lockbox
-                    </TabsTrigger>
-                    <TabsTrigger
-                      value="flows"
-                      activeTab={activeTab}
-                      setActiveTab={setActiveTab}
-                    >
-                      Flows
-                    </TabsTrigger>
+                    {tabLabels.map((label) => (
+                      <TabsTrigger
+                        key={label}
+                        value={label.toLowerCase()}
+                        activeTab={activeTab}
+                        setActiveTab={setActiveTab}
+                      >
+                        {label}
+                      </TabsTrigger>
+                    ))}
                   </TabsList>
 
                   <TabsContent
@@ -361,176 +320,37 @@ function ZcashChart(props: ZcashChartProps) {
                     activeTab={activeTab}
                     ref={props.divChartRef}
                   >
-                    <div className="space-y-6">
-                      <div className="flex mt-12">
-                        <h3 className="text-lg font-semibold mb-4 flex-1">
-                          Shielded Supply Overview
-                        </h3>
-                        <CardContent className="flex items-center flex-col lg:flex-row gap-4">
-                          <div className="flex justify-center items-center gap-2">
-                            <label className="text-sm font-medium">Year</label>
-                            <Select
-                              value={selectedYear}
-                              onValueChange={setSelectedYear}
-                            >
-                              <SelectTrigger className="w-32">
-                                <SelectValue />
-                              </SelectTrigger>
-                              <SelectContent>
-                                {getAvailableYears().map((year) => (
-                                  <SelectItem
-                                    key={year}
-                                    value={year.toString()}
-                                    className="hover:cursor-pointer bg-slate-50 dark:bg-slate-800"
-                                  >
-                                    {year === "all" ? "All" : year}
-                                  </SelectItem>
-                                ))}
-                              </SelectContent>
-                            </Select>
-                          </div>
-
-                          <div className="flex justify-center items-center gap-2">
-                            <label className="text-sm font-medium">
-                              Total Supply
-                            </label>
-                            <div className="w-fit">
-                              {calculateTotalSupply(
-                                selectedYear
-                              ).toLocaleString()}{" "}
-                              ZEC
-                            </div>
-                          </div>
-                        </CardContent>
-                      </div>
-                      <ResponsiveContainer width="100%" height={400}>
-                        {loading ? (
-                          <div className="flex justify-center items-center">
-                            <Spinner />
-                          </div>
-                        ) : (
-                          <AreaChart data={combinedPoolData}>
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="close" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Area
-                              type="monotone"
-                              dataKey="sprout"
-                              stackId="1"
-                              stroke="hsl(var(--chart-1))"
-                              fill="hsl(var(--chart-1))"
-                              name="Sprout Pool"
-                            />
-                            <Area
-                              type="monotone"
-                              dataKey="sapling"
-                              stackId="1"
-                              stroke="hsl(var(--chart-2))"
-                              fill="hsl(var(--chart-2))"
-                              name="Sapling Pool"
-                            />
-                            <Area
-                              type="monotone"
-                              dataKey="orchard"
-                              stackId="1"
-                              stroke="hsl(var(--chart-3))"
-                              fill="hsl(var(--chart-3))"
-                              name="Orchard Pool"
-                            />
-                          </AreaChart>
-                        )}
-                      </ResponsiveContainer>
-                    </div>
+                    <ShieldedSupplyChart
+                      calculateTotalSupply={calculateTotalSupply}
+                      combinedPoolData={combinedPoolData}
+                      difficulty={difficulty}
+                      getAvailableYears={getAvailableYears}
+                      loading={loading}
+                      selectedYear={selectedYear}
+                      setSelectedYear={setSelectedYear}
+                    />
                   </TabsContent>
 
                   <TabsContent value="difficulty" activeTab={activeTab}>
-                    <div className="space-y-6">
-                      <div className="flex mt-12">
-                        <h3 className="text-lg font-semibold mb-4 flex-1">
-                          Mining Difficulty
-                        </h3>
-                      </div>
-                      <ResponsiveContainer width="100%" height={400}>
-                        <LineChart data={difficulty}>
-                          <CartesianGrid strokeDasharray="3 3" />
-                          <XAxis dataKey="Date" />
-                          <YAxis />
-                          <Tooltip />
-                          <Legend />
-                          <Line
-                            type="monotone"
-                            dataKey="Difficulty"
-                            stroke="hsl(var(--chart-4))"
-                            strokeWidth={2}
-                          />
-                        </LineChart>
-                      </ResponsiveContainer>
-                    </div>
+                    <DifficultyChart difficulty={difficulty} />
                   </TabsContent>
 
                   <TabsContent value="issuance" activeTab={activeTab}>
-                    {activeTab === "issuance" && <IssuanceChart />}
+                    {activeTab === "issuance" && (
+                      <IssuanceChart url={DATA_URL.issuanceUrl} />
+                    )}
                   </TabsContent>
 
                   <TabsContent value="lockbox" activeTab={activeTab}>
-                    <div className="space-y-6">
-                      <div className="flex mt-12">
-                        <h3 className="text-lg font-semibold mb-4 flex-1">
-                          Lockbox Activity
-                        </h3>
-                      </div>
-
-                      <ResponsiveContainer width="100%" height={400}>
-                        <AreaChart data={lockboxData}>
-                          <CartesianGrid strokeDasharray="3 3" />
-                          <XAxis dataKey="Date" />
-                          <YAxis />
-                          <Tooltip />
-                          <Legend />
-                          <Area
-                            type="monotone"
-                            dataKey="lockbox"
-                            stroke="hsl(var(--chart-1))"
-                            fill="hsl(var(--chart-1))"
-                            fillOpacity={0.6}
-                          />
-                        </AreaChart>
-                      </ResponsiveContainer>
-                    </div>
+                    <LockboxChart lockboxData={lockboxData} />
                   </TabsContent>
 
                   <TabsContent value="flows" activeTab={activeTab}>
-                    <div className="space-y-6">
-                      <div className="flex mt-12">
-                        <h3 className="text-lg font-semibold mb-4 flex-1">
-                          Net Sapling & Orchard Flow
-                        </h3>
-                      </div>
-
-                      <ResponsiveContainer width="100%" height={400}>
-                        <LineChart data={netInOutflowData}>
-                          <CartesianGrid strokeDasharray="3 3" />
-                          <XAxis dataKey="Date" />
-                          <YAxis />
-                          <Tooltip />
-                          <Legend />
-                          <Line
-                            type="monotone"
-                            dataKey="Net Sapling Flow"
-                            stroke="hsl(var(--chart-2))"
-                            strokeWidth={2}
-                          />
-                          <Line
-                            type="monotone"
-                            dataKey="Net Orchard Flow"
-                            stroke="hsl(var(--chart-3))"
-                            strokeWidth={2}
-                          />
-                        </LineChart>
-                      </ResponsiveContainer>
-                    </div>
+                    {/* <NetFlowChart netInOutflowData={netInOutflowData} /> */}
+                    <NetInflowsOutflowsChart
+                      color="red"
+                      dataUrl={DATA_URL.netInflowsOutflowsUrl}
+                    />
                   </TabsContent>
 
                   <div>
