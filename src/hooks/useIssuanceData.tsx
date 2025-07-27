996a6dd3a4edbebbd0d68bd02ee82f417cc6b77d
amochuko <7204868+amochuko@users.noreply.github.com>
2025-07-27 13:28:12 +0100

    update: add loading state

--- src/hooks/useIssuanceData.tsx
+++ src/hooks/useIssuanceData.tsx
@@ -2,14 +2,17 @@ import { Issuance } from "@/lib/chart/types";
 import { useEffect, useState } from "react";
 
 export const useIssuanceData = (url: string) => {
+  const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    setLoading(true);
+
     fetch(url)
       .then((res) => res.json())
       .then((raw) => {
         const parsed = raw
-          .filter((_:any, i: number) => i % 7 === 0) // weekly sampling
+          .filter((_: any, i: number) => i % 7 === 0) // weekly sampling
           .map((d: Issuance) => ({
             date: d.Date.split(" ")[0],
             issuance: parseFloat(d["ZEC Issuance"]),
@@ -17,8 +20,9 @@ export const useIssuanceData = (url: string) => {
           }));
 
         setData(parsed);
+        setLoading(false);
       });
   }, []);
 
-  return data;
+  return {data, loading};
 };
