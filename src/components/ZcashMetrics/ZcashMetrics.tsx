12599d73626373736c6b45bcc10a2263c5c0c7d2
amochuko <7204868+amochuko@users.noreply.github.com>
2025-07-27 13:13:46 +0100

    update: pull state in

--- src/components/ZcashMetrics/ZcashMetrics.tsx
+++ src/components/ZcashMetrics/ZcashMetrics.tsx
@@ -1,52 +1,104 @@
 import { useInMobile } from "@/hooks/useInMobile";
-import { BlockchainInfo } from "@/lib/chart/types";
-import { MetricCard } from "./MetricCard";
+import { DATA_URL } from "@/lib/chart/data-url";
+import {
+  getBlockchainData,
+  getShieldedTxCount,
+  getZcashCirculationCount,
+} from "@/lib/chart/helpers";
+import { BlockchainInfo, ShieldedTxCount } from "@/lib/chart/types";
+import { useEffect, useState } from "react";
+import { MetricCard, MetricCardSkeleton } from "./MetricCard";
 
-interface ZcashStatisticsPorps {
-  blockchainInfo: BlockchainInfo;
-  circulation: number;
-  shieldedTxCount: any[];
-}
+interface ZcashStatisticsPorps {}
 
 /* Metrics */
 export function ZcashMetrics(props: ZcashStatisticsPorps) {
   const isMobile = useInMobile();
 
+  const [loading, setLoading] = useState(true);
+  const [circulation, setCirculation] = useState<number | null>(null);
+  const [blockchainInfo, setBlockchainInfo] = useState<BlockchainInfo | null>();
+  const [shieldedTxCount, setShieldedTxCount] = useState<
+    ShieldedTxCount[] | null
+  >([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchAllData = async () => {
+      setLoading(true);
+
+      try {
+        const [chainData, circulationInfo, shieldedTxCount] = await Promise.all(
+          [
+            getBlockchainData(DATA_URL.blockchairUrl, controller.signal),
+            getZcashCirculationCount(
+              DATA_URL.blockchainInfoUrl,
+              controller.signal
+            ),
+            getShieldedTxCount(DATA_URL.shieldedTxCountUrl, controller.signal),
+          ]
+        );
+
+        if (chainData) {
+          setBlockchainInfo(chainData);
+        }
+
+        if (circulationInfo) {
+          setCirculation(circulationInfo);
+        }
+
+        if (shieldedTxCount) {
+          setShieldedTxCount(shieldedTxCount);
+        }
+
+        setLoading(false);
+      } catch (err) {
+        setLoading(false);
+        console.error("Error fetching dashboard data:", err);
+      }
+    };
+
+    fetchAllData();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
   const metricsObj = [
     {
       label: "Market Cap",
-      value: `$${
-        props.blockchainInfo?.market_cap_usd.toLocaleString() ?? "NA"
-      }`,
+      value: `$${blockchainInfo?.market_cap_usd.toLocaleString() ?? "N/A"}`,
     },
     {
       label: "Circulation",
-      value: `${props.circulation?.toLocaleString() ?? "N/A"} ZEC`,
+      value: `${circulation?.toLocaleString() ?? "N/A"} ZEC`,
     },
     {
       label: "Market Price (USD)",
-      value: `$${props.blockchainInfo?.market_price_usd.toFixed(2) ?? "NA"}`,
+      value: `$${blockchainInfo?.market_price_usd.toFixed(2) ?? "N/A"}`,
     },
     {
       label: "Market Price (BTC)",
       value: isMobile
-        ? Number(props.blockchainInfo?.market_price_btc).toFixed(4) ?? "NA"
-        : Number(props.blockchainInfo?.market_price_btc).toFixed(8) ?? "NA",
+        ? Number(blockchainInfo?.market_price_btc).toFixed(4) ?? "N/A"
+        : Number(blockchainInfo?.market_price_btc).toFixed(8) ?? "N/A",
     },
     {
       label: "Blocks",
-      value: props.blockchainInfo?.blocks.toLocaleString() ?? "NA",
+      value: blockchainInfo?.blocks.toLocaleString() ?? "NA",
     },
     {
       label: "24h Transactions",
-      value: props.blockchainInfo?.transactions_24h.toLocaleString() ?? "NA",
+      value: blockchainInfo?.transactions_24h.toLocaleString() ?? "N/A",
     },
     {
       label: "Shielded TX (24h)",
-      value: props.shieldedTxCount?.length
-        ? `Sapling: ${props.shieldedTxCount
+      value: shieldedTxCount?.length
+        ? `Sapling: ${shieldedTxCount
             .at(-1)!
-            .sapling.toLocaleString()} | Orchard: ${props.shieldedTxCount
+            .sapling.toLocaleString()} | Orchard: ${shieldedTxCount
             .at(-1)!
             .orchard.toLocaleString()}`
         : "N/A",
@@ -59,9 +111,13 @@ export function ZcashMetrics(props: ZcashStatisticsPorps) {
         Zcash Metrics
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">
-        {metricsObj.map(({ label, value }) => (
-          <MetricCard label={label} value={value} key={label} />
-        ))}
+        {loading
+          ? metricsObj.map(({ label, value }) => (
+              <MetricCardSkeleton key={label} />
+            ))
+          : metricsObj.map(({ label, value }) => (
+              <MetricCard label={label} value={value} key={label} />
+            ))}
       </div>
     </div>
   );
