292a24fbe2ddfeba2f02dc54efce165a3735e7c6
amochuko <7204868+amochuko@users.noreply.github.com>
2025-08-01 23:18:12 +0100

    update: add chart footer

---
+++ src/components/Charts/ChartFooter.tsx
@@ -0,0 +1,39 @@
+import { ExportButton } from "./ExportButton";
+import SupplyDataLastUpdated from "./LastUpdated";
+
+type ChartFooterProps = {
+  lastUpdatedDate: Date;
+  handleSaveToPng: any;
+  pngLabel: any;
+  data?: [];
+};
+
+const ChartFooter = (props: ChartFooterProps) => {
+  console.log({ lastUpdated: props.lastUpdatedDate });
+
+  return (
+    <>
+      {props.lastUpdatedDate && (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            marginTop: 48,
+          }}
+        >
+          <SupplyDataLastUpdated lastUpdated={props.lastUpdatedDate} />
+
+          <ExportButton
+            handleSaveToPng={props.handleSaveToPng}
+            selectedPool={props.pngLabel}
+            supplies={props.data}
+            selectedTool={undefined}
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ChartFooter;
