9294f9ae2007c4edcd89a8a99a61e010b9d9eb47
amochuko <7204868+amochuko@users.noreply.github.com>
2025-08-01 23:19:19 +0100

    update: refactor to accomodate new chart

--- src/hooks/useExportDashboardAsPNG.tsx
+++ src/hooks/useExportDashboardAsPNG.tsx
@@ -14,25 +14,29 @@ const useExportDashboardAsPNG = () => {
 
   const handleSaveToPng = async (
     poolType: string,
-    poolData: Record<string, { timestamp: string; supply: number } | null>,
+    poolData:
+      | Record<string, { timestamp: string; supply: number } | null>
+      | string,
     toolType: string
-  ) => {    
+  ) => {
     const poolQty = document.createTextNode("");
 
-    if (poolType == PoolsType.sprout) {
-      poolQty.textContent = `${poolData[
-        "sproutSupply"
-      ]?.supply.toLocaleString()} ZEC in Sprout Pool`;
-    } else if (poolType == PoolsType.ord) {
-      poolQty.textContent = `${poolData[
-        "orchardSupply"
-      ]?.supply.toLocaleString()} ZEC in Orchard Pool`;
-    } else if (poolType == PoolsType.sap) {
-      poolQty.textContent = `${poolData[
-        "saplingSupply"
-      ]?.supply.toLocaleString()} ZEC in Sapling Pool`;
-    } else {
-      poolQty.textContent = "";
+    if (typeof poolData != "string") {
+      if (poolType == PoolsType.sprout) {
+        poolQty.textContent = `${poolData[
+          "sproutSupply"
+        ]?.supply.toLocaleString()} ZEC in Sprout Pool`;
+      } else if (poolType == PoolsType.ord) {
+        poolQty.textContent = `${poolData[
+          "orchardSupply"
+        ]?.supply.toLocaleString()} ZEC in Orchard Pool`;
+      } else if (poolType == PoolsType.sap) {
+        poolQty.textContent = `${poolData[
+          "saplingSupply"
+        ]?.supply.toLocaleString()} ZEC in Sapling Pool`;
+      } else {
+        poolQty.textContent = "";
+      }
     }
 
     const span = document.createElement("span");
@@ -52,10 +56,12 @@ const useExportDashboardAsPNG = () => {
       const link = document.createElement("a");
 
       link.href = canvas.toDataURL("image/png");
-      if(toolType == 'supply') link.download = `zcash-${poolType}-pool-chart.png`;
-      else link.download = `zcash-${poolType}-transaction-chart.png`;
+      // if(toolType == 'supply') link.download = `zcash-${poolType}-pool-chart.png`;
+      // else link.download = `zcash-${poolType}-transaction-chart.png`;
+
+      link.download = `zcash-${poolType}-transaction-chart.png`;
       link.click();
-      
+
       divChartRef.current?.removeChild(span);
     } catch (err) {
       console.error("Error saving chart: ", err);
