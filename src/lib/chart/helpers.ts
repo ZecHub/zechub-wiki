52cc70e353e86042adec28c5771a20a94077e1ac
amochuko <7204868+amochuko@users.noreply.github.com>
2025-08-03 19:19:55 +0100

    update: formatNumberShort

--- src/lib/chart/helpers.ts
+++ src/lib/chart/helpers.ts
@@ -268,3 +268,10 @@ export const getColorForPool = (poolKey: PoolType): string => {
       return "#999999"; // fallback gray
   }
 };
+
+export function formatNumberShort(num: number): string {
+  if (num >= 1_000_000_000) return `${(num / 1_000_000_000).toFixed(1)}B`;
+  if (num >= 1_000_000) return `${(num / 1_000_000).toFixed(1)}M`;
+  if (num >= 1_000) return `${(num / 1_000).toFixed(1)}k`;
+  return num.toString();
+}
