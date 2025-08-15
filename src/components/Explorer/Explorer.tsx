f3ea9018e609b8ae36740b9034efdb7baf88cca9
amochuko <7204868+amochuko@users.noreply.github.com>
2025-08-15 02:11:35 +0100

    fix: adjust layout

--- src/components/Explorer/Explorer.tsx
+++ src/components/Explorer/Explorer.tsx
@@ -1,14 +1,23 @@
-import ListExplorer from "./ListExplorer";
 import { exploreFolders } from "@/constants/exploreFolders";
+import ListExplorer from "./ListExplorer";
 
 const Explorer = () => {
   return (
-    <div className="flex w-full justify-items-center items-center flex-col h-auto">
-      <h1 className="text-3xl my-5 font-bold text-center">Explore Zcash</h1>
+    <div className="flex w-full justify-items-center items-center flex-col h-auto border border-slate-200 dark:border-slate-700 rounded-md">
+      <h4 className="text-3xl font-bold text-center my-12 text-gray-900 dark:text-white">
+        Explore Zcash
+      </h4>
+
       <div className="w-full grid grid-cols-1 space-x-4 space-y-2 md:grid-cols-3 md:gap-3 justify-items-center mt-4 p-2">
         {exploreFolders &&
           exploreFolders.map(({ img, description, name, url }, i) => (
-            <ListExplorer key={name+i} image={img} description={description} name={name} url={url} />
+            <ListExplorer
+              key={name + i}
+              image={img}
+              description={description}
+              name={name}
+              url={url}
+            />
           ))}
       </div>
     </div>
