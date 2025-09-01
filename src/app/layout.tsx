80d7fce24237732cb8ceb4fe40b2e0b281fd3b88
amochuko <7204868+amochuko@users.noreply.github.com>
2025-09-01 18:52:28 +0100

    update: add progress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import { Footer, Navigation } from "@/components";
+import ProgressBar from "@/components/UI/ProgressBar";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: 'ZecHub Wiki',
-//   description: 'An open source education hub for Zcash',
-//   icons: 'https://i.ibb.co/ysPDS9Q/Zec-Hub-blue-globe.png',
-// };
+export const metadata: Metadata = {
+  title: 'ZecHub Wiki',
+  description: 'An open source education hub for Zcash',
+  icons: 'https://i.ibb.co/ysPDS9Q/Zec-Hub-blue-globe.png',
+};
 
 export default function RootLayout({
   children,
@@ -22,7 +24,9 @@ export default function RootLayout({
       <body className={`px-0  ${inter.className}`}>
         {/* <div className="container mx-auto min-h-screen flex flex-col"> */}
         <div className="min-h-screen mx-auto">
+          <ProgressBar />
           <Navigation />
+
           <div className=" flex flex-col justify-between flex-grow">
             <div style={{ margin: "0 0 48px 0" }}>{children}</div>
           </div>
