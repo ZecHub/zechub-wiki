9fcd7f257421416fe4cfa52910d147b97e7a502b
amochuko <7204868+amochuko@users.noreply.github.com>
2025-09-06 20:30:01 +0100

    fix: rename file

--- src/app/payment-processors/page.tsx
+++ src/app/payment-processors/page.tsx
@@ -1,23 +1,15 @@
 // src/app/payment-processors/page.tsx
-import React from "react";
-import Image from "next/image";
-import {
-  getFileContent,
-  getFileContentCached,
-  getRoot,
-  getRootCached,
-} from "@/lib/authAndFetch";
-import { getBanner } from "@/lib/helpers";
-import { genMetadata } from "@/lib/helpers";
-import { Metadata } from "next";
 import PaymentProcessorList from "@/components/PaymentProcessor/PaymentProcessorList";
+import { getFileContentCached } from "@/lib/authAndFetch";
+import { genMetadata, getBanner } from "@/lib/helpers";
 import { parseProcessorMarkdown } from "@/lib/parseProcessorMarkdown";
-
+import { Metadata } from "next";
+import Image from "next/image";
 const imgUrl = getBanner(`using-zcash`);
 
 export const metadata: Metadata = genMetadata({
   title: "Payment Processors | Zechub",
-  url: "https://zechub.wiki/payment-processors",
+  url: "https://zechub.wiki/using-zcash/payment-processors",
   image: imgUrl,
 });
 
@@ -25,20 +17,29 @@ export default async function Page(props: {
   params: Promise<{ slug: string }>;
 }) {
   const params = await props.params;
-  const { slug } = params;
+
+  // const { slug } = params;
+  // const { slug } = await props.params;
+
   const url = "site/Using_Zcash/Payment_Processors.md";
-
-  // const markdown = await getFileContent(url);
-  const markdown = await getFileContentCached(url);
-  console.log("Markdown Content:", markdown); // Check fetched markdown content
-
-  const content = markdown ? markdown : "No Data or Wrong file";
+  // const url = getDynamicRoute(slug);
+  // const urlRoot = `/site/${slug[0]}`;
   const urlRoot = "site/using-zcash";
-  // const roots = await getRoot(urlRoot);
-  const roots = await getRootCached(urlRoot);
+
+  const [markdown, roots] = await Promise.all([
+    getFileContentCached(url),
+    // getRootCached(urlRoot),
+    "",
+  ]);
+  const content = markdown ? markdown : "No Data or Wrong file";
 
   const paymentProcessors = parseProcessorMarkdown(content);
-  console.log("Parsed Payment Processors:", paymentProcessors); // Check parsed markdown output
+  console.log("payment-processors: ", {
+    params,
+    roots,
+    content,
+    // paymentProcessors,
+  });
 
   return (
     <main>
@@ -58,11 +59,8 @@ export default async function Page(props: {
           roots && roots.length > 0 ? "md:flex-row md:space-x-5" : "md:flex-col"
         } h-auto w-full py-5`}
       >
-        <section className="h-auto w-full">
-          <h1>Payment Processor</h1>
-          <div>
-            {/* <PaymentProcessorList allProcessors={paymentProcessors} /> */}
-          </div>
+        <section className="h-auto w-full px-4">
+          <PaymentProcessorList allProcessors={paymentProcessors} />
         </section>
       </div>
     </main>
