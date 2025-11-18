9ca91b7fc46a65eff757c0461ab22817773b270e
Qdev <131249545+Henryquincy@users.noreply.github.com>
2025-11-18 23:09:24 +0100

    Update data-url.ts
    Update data-url.ts - Add Block Fees

--- src/lib/chart/data-url.ts
+++ src/lib/chart/data-url.ts
@@ -11,7 +11,7 @@ export const DATA_URL = {
   lockboxUrl: "/data/zcash/lockbox.json",
   shieldedTxCountUrl: "/data/zcash/shieldedtxcount.json",
   issuanceUrl: "/data/zcash/issuance.json",
-  zcashShieldedStatsUrl : "/data/zcash/shieldedStatsJSON",
+  zcashShieldedStatsUrl: "/data/zcash/shieldedStatsJSON",
   shieldedUrl:
     "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/shielded_supply.json",
   namadaSupplyUrl: "/data/namada/namada_supply.json",
@@ -19,27 +19,43 @@ export const DATA_URL = {
   blockchairUrl:
     "https://api.blockchair.com/zcash/stats?key=A___NwjLQ29USRVGxzBQd2QoFvxSRNSk",
   namadaRewardUrl: "/data/namada/namada_rewards_rate.json",
+  blockFeesUrl: "/data/zcash/blockFeesZEC.json",
 } as const;
-
 
 export const DATE_URL = {
-  defaultUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/shielded_supply.json&per_page=1",
-  sproutUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/sprout_supply.json&per_page=1",
-  saplingUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/sapling_supply.json&per_page=1",
-  orchardUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/orchard_supply.json&per_page=1",
-  txsummaryUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/transaction_summary.json&per_page=1",
-  transparentSupplyUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/transparent_supply.json&per_page=1",
-  netInflowsOutflowsUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/netinflowoutflow.json&per_page=1",
-  nodecountUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/nodecount.json&per_page=1",
-  difficultyUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/difficulty.json&per_page=1",
-  lockboxUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/lockbox.json&per_page=1",
-  shieldedTxCountUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/shieldedtxcount.json&per_page=1",
-  issuanceUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/issuance.json&per_page=1",
-  shieldedUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/shielded_supply.json&per_page=1",
-  namadaSupplyUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/namada/namada_supply.json&per_page=1",
+  defaultUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/shielded_supply.json&per_page=1",
+  sproutUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/sprout_supply.json&per_page=1",
+  saplingUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/sapling_supply.json&per_page=1",
+  orchardUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/orchard_supply.json&per_page=1",
+  txsummaryUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/transaction_summary.json&per_page=1",
+  transparentSupplyUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/transparent_supply.json&per_page=1",
+  netInflowsOutflowsUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/netinflowoutflow.json&per_page=1",
+  nodecountUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/nodecount.json&per_page=1",
+  difficultyUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/difficulty.json&per_page=1",
+  lockboxUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/lockbox.json&per_page=1",
+  shieldedTxCountUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/shieldedtxcount.json&per_page=1",
+  issuanceUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/issuance.json&per_page=1",
+  shieldedUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/shielded_supply.json&per_page=1",
+  namadaSupplyUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/namada/namada_supply.json&per_page=1",
   blockchainInfoUrl: "/api/blockchain-info",
-  blockchairUrl: "https://api.blockchair.com/zcash/stats?key=A___NwjLQ29USRVGxzBQd2QoFvxSRNSk",
-  namadaRewardUrl: "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/namada/namada_rewards_rate.json&per_page=1",
-  zcashShieldedStatsUrl : "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/shieldedStatsJSON&per_page=1",
+  blockchairUrl:
+    "https://api.blockchair.com/zcash/stats?key=A___NwjLQ29USRVGxzBQd2QoFvxSRNSk",
+  namadaRewardUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/namada/namada_rewards_rate.json&per_page=1",
+  zcashShieldedStatsUrl:
+    "https://api.github.com/repos/ZecHub/zechub-wiki/commits?path=public/data/zcash/shieldedStatsJSON&per_page=1",
 } as const;
-
