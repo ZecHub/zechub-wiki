a1df9520464f6a2333f20073776c29b752a86623
amochuko <7204868+amochuko@users.noreply.github.com>
2025-12-15 18:34:47 +0100

    add zcash pool visualizer page

---
+++ src/app/zcash-pool-visualizer/page.tsx
@@ -0,0 +1,12 @@
+import { ZcashPoolVisualizer } from "@/components/visualizer/zcash-pool-visualizer";
+import { genMetadata } from "@/lib/helpers";
+import { Metadata } from "next";
+
+export const metadata: Metadata = genMetadata({
+  title: "Zechub Tutorial",
+  url: "https://zechub.wiki/zcash-pool-visualizer",
+});
+
+export default function ZcashPoolVisualizerPage() {
+  return <ZcashPoolVisualizer />;
+}
