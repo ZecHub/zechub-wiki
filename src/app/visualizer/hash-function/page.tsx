c8ca0ecefd7179309965e0c3840fcfdc276b411a
amochuko <7204868+amochuko@users.noreply.github.com>
2026-01-07 12:26:35 +0100

    add hash function visulalizer

---
+++ src/app/visualizer/hash-function/page.tsx
@@ -0,0 +1,5 @@
+import { HashFunctionVisualizer } from "@/components/visualizer/cryptographic-fingerprints-visualizer";
+
+export default function HashFunctionContent(){
+    return <HashFunctionVisualizer/>
+}
