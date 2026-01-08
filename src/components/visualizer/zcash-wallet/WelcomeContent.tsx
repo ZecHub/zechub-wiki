fd0c0499b1f5e43715c062369419cf6cf90a19ad
amochuko <7204868+amochuko@users.noreply.github.com>
2026-01-08 17:56:10 +0100

    replace with dynamic wallet data

--- src/components/visualizer/zcash-wallet/WelcomeContent.tsx
+++ src/components/visualizer/zcash-wallet/WelcomeContent.tsx
@@ -1,25 +1,15 @@
+"use client";
 import { useDarkModeContext } from "@/hooks/useDarkModeContext";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import ZashiIconBlack from "../../../assets/brand/Wallets/Zashi/PNG/ZashiIconBlack.png";
-import ZashIconWhite from "../../../assets/brand/Wallets/Zashi/PNG/ZashiIconWhite.png";
+import { WalletInfo } from "./index";
 import { Stage } from "./types";
 
-export const WelcomeContent = ({ stage }: { stage: Stage }) => {
+export const WelcomeContent = (props: {
+  stage: Stage;
+  wallets: WalletInfo[];
+}) => {
   const { dark } = useDarkModeContext();
-
-  const walletInfo = [
-    {
-      label: "Zashi",
-      icon: dark ? ZashIconWhite.src : ZashiIconBlack.src,
-      description: "",
-    },
-    {
-      label: "YWallet",
-      icon: "/zwallets.png",
-      description: "",
-    },
-  ];
 
   return (
     <div className="space-y-12">
@@ -28,30 +18,24 @@ export const WelcomeContent = ({ stage }: { stage: Stage }) => {
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ delay: 0.3 }}
-        className="flex justify-center gap-12 my-16"
+        className="flex justify-center gap-8 my-16"
       >
-        {walletInfo.map((w, idx) => (
+        {props.wallets.slice(0, 6).map((w, idx) => (
           <motion.div
-            key={idx + w.label}
-            animate={{ y: [0, -10, 0] }}
-            transition={{
-              duration: 2,
-              repeat: Infinity,
-              delay: 0.4 * idx + 0.3,
-            }}
+            key={idx + w.title}
             className="flex flex-col items-center gap-2"
           >
-            <div className="p-4 rounded-xl bg-pool-transparent/10 border border-pool-transparent/30">
+            <div className="p-2 rounded-xl bg-pool-transparent/10 border border-pool-transparent/30">
               <Image
                 priority
-                src={w.icon}
-                alt={w.label}
-                width={32}
-                height={32}
-                className="w-8 h-8 text-pool-transparent"
+                src={w.imageUrl.trimEnd()}
+                alt={w.title}
+                width={64}
+                height={64}
+                // className="w-12 h-12 text-pool-transparent"
               />
             </div>
-            <span className="text-xs text-muted-foreground">{w.label}</span>
+            <span className="text-xs text-muted-foreground">{w.title}</span>
           </motion.div>
         ))}
       </motion.div>
