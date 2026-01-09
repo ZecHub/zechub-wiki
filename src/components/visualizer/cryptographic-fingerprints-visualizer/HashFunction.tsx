0ac214394aeafcaa526d5e18c9eae0d31e855f39
amochuko <7204868+amochuko@users.noreply.github.com>
2026-01-09 09:12:55 +0100

    update switch case

--- src/components/visualizer/cryptographic-fingerprints-visualizer/HashFunction.tsx
+++ src/components/visualizer/cryptographic-fingerprints-visualizer/HashFunction.tsx
@@ -1,25 +1,25 @@
-
-import { HashFunctionContent } from "./HashFunctionContent";
+import { CollisionContent } from "./CollisionContent";
 import { IntegrityContent } from "./IntegrityContent";
 import { IntroContent } from "./IntroContent";
+import { IrreversibleContent } from "./IrreversibleContent";
+import { OtherPropertiesContent } from "./OtherPropertiesContent";
 import { Stage } from "./types";
 
 interface HashFunctionContentProps {
   stage: Stage;
 }
 
-export type HashType =
-  | "intro"
-  | "integrity"
-  | "irreversible"
-  | "collision"
-  | "properties";
-
 export const HashFunction = (props: HashFunctionContentProps) => {
   switch (props.stage.hashType) {
     case "intro":
       return <IntroContent stage={props.stage} />;
-    case 'integrity':
-      return <IntegrityContent/>
+    case "integrity":
+      return <IntegrityContent />;
+    case "irreversible":
+      return <IrreversibleContent />;
+    case "collision":
+      return <CollisionContent />;
+    case "properties":
+      return <OtherPropertiesContent />;
   }
 };
