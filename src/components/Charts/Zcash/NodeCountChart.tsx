994c344681a8594b30731e65bb18e3bc68c86a30
onajifortune <onajifortunejnr@gmail.com>
2026-01-11 14:57:38 +0100

    Fixed type error 2

--- src/components/Charts/Zcash/NodeCountChart.tsx
+++ src/components/Charts/Zcash/NodeCountChart.tsx
@@ -137,7 +137,7 @@ const NodeCountChart = (props: NodeCountChartProps) => {
               <circle
                 cx={cx}
                 cy={cy}
-                key={index + cx}
+                key={`node-dot-${index}`}
                 r={4}
                 fill="#3b82f6" // background color
                 stroke="white" // outer ring
