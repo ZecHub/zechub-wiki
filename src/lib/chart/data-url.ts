84288ca43fd6b288cd825b27a21f34f1a386422d
squirrel <47972284+zksquirrel@users.noreply.github.com>
2026-01-25 14:09:09 +0000

    Update data-url.ts

--- src/lib/chart/data-url.ts
+++ src/lib/chart/data-url.ts
@@ -18,7 +18,7 @@ export const DATA_URL = {
   namadaSupplyUrl: "/data/namada/namada_supply.json",
   blockchainInfoUrl: "/api/blockchain-info",
   blockchairUrl:
-    "https://api.blockchair.com/zcash/stats?key=A___akeIQZjpBEtzalr10ajq4BhxTfSc",
+    "https://api.blockchair.com/zcash/stats?key=A___iPohHSSJXXe8n8shE72yN0OFz0zp",
   namadaRewardUrl: "/data/namada/namada_rewards_rate.json",
   blockFeesUrl: "/data/zcash/blockFeesZEC.json",
   networkSolpsUrl: "/data/zcash/networksolps.json",
