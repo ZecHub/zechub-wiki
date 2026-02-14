64d0936e0ee676b29d7acba272b8342135489606
amochuko <7204868+amochuko@users.noreply.github.com>
2026-02-14 02:13:12 +0100

    parse data

--- src/app/governance/lib/github.ts
+++ src/app/governance/lib/github.ts
@@ -1,29 +1,54 @@
-import { config } from "./config";
+import * as cheerio from "cheerio";
+import { appConfig } from "./appConfig";
 
 export interface ZIPData {
-  number: number;
+  zipNumber: string;
   title: string;
   status: string;
-  type: string;
-  authors: string;
+  type?: string;
+  authors?: string;
   url: string;
 }
 
-interface GitHubFile {
-  name: string;
-  path: string;
-  download_url: string;
-}
-
 export async function fetchZIPs(): Promise<ZIPData[]> {
-  const res = await fetch(config.zipUrl, {
-    headers: { Accept: "application/vnd.github.v3+json" },
-  });
+  try {
+    const res = await fetch(appConfig.zipRawUrl);
 
-  if (!res.ok) throw new Error("Failed to fetch ZIPs from GitHub");
+    if (!res.ok) throw new Error("Failed to fetch ZIPs from GitHub");
 
-  const files: GitHubFile[] = await res.json();
-  // const zipsFiles = f
+    const content = await res.text();
 
-  return [];
+    const startIndex = content.indexOf("Index of ZIPs");
+    if (startIndex === -1) {
+      throw new Error("Index of ZIPs section not found!");
+    }
+
+    const section = content.slice(startIndex);
+    const tableMatch = section.match(/<table[\s\S]*?<\/table>/);
+    if (!tableMatch) {
+      throw new Error("Table not found!");
+    }
+
+    const $ = cheerio.load(tableMatch[0]);
+
+    const rows: ZIPData[] = [];
+
+    $("table tr").each((_, row) => {
+      const cols = $(row).find("td");
+
+      if (cols.length === 3) {
+        rows.push({
+          zipNumber: $(cols[0]).text().trim(),
+          title: $(cols[1]).text().trim(),
+          status: $(cols[2]).text().trim(),
+          url: appConfig.zipUrl + $(cols[1]).find("a").attr("href"),
+        });
+      }
+    });
+
+    return rows;
+  } catch (e: any) {
+    console.error("Failed to fetch ZIPs file");
+    throw new Error(e);
+  }
 }
