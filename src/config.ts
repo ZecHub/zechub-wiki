12d6feb632d781f504b65ad589ce84b7f26c643e
amochuko <7204868+amochuko@users.noreply.github.com>
2026-02-20 09:24:29 +0100

    add zcgrants config

--- src/config.ts
+++ src/config.ts
@@ -41,10 +41,29 @@ const NOTIFICATION_PERMISSION = "zechub-wiki-notification-permission";
 const ZIPs_RAW_URL =
   "https://raw.githubusercontent.com/zcash/zips/main/README.rst";
 const ZIPs_URL = "https://github.com/zcash/zips/blob/main/";
+// const SPREADSHEET_ID = "1FQ28rDCyRW0TiNxrm3rgD8ai2KGUsXAjPieQmI1kKKg";
+// const SHEET_ID = "803214474";
+// const ZCG_GRANTS_GOOGLE_SHEET_URL =
+//   `https://docs.google.com/spreadsheets/d/${SPREADSHEET_ID}/edit?gid=${SHEET_ID}#gid=${SHEET_ID}`;
+
+const GOOGLE_ZCG_API_KEY = process.env.GOOGLE_ZCG_API_KEY;
+const GOOGLE_ZCG_SERVICE_ACCOUNT_EMAIL =
+  process.env.GOOGLE_ZCG_SERVICE_ACCOUNT_EMAIL;
+const GOOGLE_ZCG_OAUTH_CLIENT_SECRET =
+  process.env.GOOGLE_ZCG_OAUTH_CLIENT_SECRET;
+const GOOGLE_ZCG_OAUTH_CLIENT_ID = process.env.GOOGLE_ZCG_OAUTH_CLIENT_ID;
+const GOOGLE_ZCG_SPREADSHEET_ID = process.env.GOOGLE_ZCG_SPREADSHEET_ID;
+const GOOGLE_ZCG_SHEET_ID = process.env.GOOGLE_ZCG_SHEET_ID;
 
 export {
   GITHUB_ID,
   GITHUB_SECRET,
+  GOOGLE_ZCG_API_KEY,
+  GOOGLE_ZCG_OAUTH_CLIENT_ID,
+  GOOGLE_ZCG_OAUTH_CLIENT_SECRET,
+  GOOGLE_ZCG_SERVICE_ACCOUNT_EMAIL,
+  GOOGLE_ZCG_SHEET_ID,
+  GOOGLE_ZCG_SPREADSHEET_ID,
   MONGO_DB_CONNECTION_STRING_DEV,
   MONGO_DB_CONNECTION_STRING_PROD,
   MONGO_DB_URI,
