51ac3e15c8aceb2c9d87369360671568acda725e
amochuko <7204868+amochuko@users.noreply.github.com>
2026-02-22 23:00:30 +0100

    update import module

--- src/app/governance/page.tsx
+++ src/app/governance/page.tsx
@@ -1,10 +1,10 @@
-import { Governance } from "./Governance";
+import { ZipAndGrantsGovernance } from "./ZipAndGrantsGovernance";
 import "./governance.css";
 
 export default function GovernancePage() {
   return (
     <div className="min-h-screen bg-background">
-      <Governance />
+      <ZipAndGrantsGovernance />
     </div>
   );
 }
