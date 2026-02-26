70d26fc0c34d284b56a23a926ab94f7a98394531
amochuko <7204868+amochuko@users.noreply.github.com>
2026-02-26 19:33:58 +0100

    add formatCurrencyToUSD

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,18 +1,24 @@
-import { contentBanners } from '@/constants/contentBanners';
-import { getRootCached } from './authAndFetch';
+import { contentBanners } from "@/constants/contentBanners";
+import { getRootCached } from "./authAndFetch";
 
 type MetadataOpts = {
-  title?: string,
-  description?: string,
-  image?: string,
-  url?: string
-}
+  title?: string;
+  description?: string;
+  image?: string;
+  url?: string;
+};
 
-export const genMetadata = ({ title, description, image, url }: MetadataOpts) => {
-  const defaultImage = '/previews/default-banner.jpg';
-  const defaultUrl = 'https://zechub.wiki';
-  const defaultTitle = 'ZecHub Wiki';
-  const defaultDescription = 'The goal of ZecHub is to provide an educational platform where community members can work together on creating, validating, and promoting content that supports the Zcash & Privacy technology ecosystems.'
+export const genMetadata = ({
+  title,
+  description,
+  image,
+  url,
+}: MetadataOpts) => {
+  const defaultImage = "/previews/default-banner.jpg";
+  const defaultUrl = "https://zechub.wiki";
+  const defaultTitle = "ZecHub Wiki";
+  const defaultDescription =
+    "The goal of ZecHub is to provide an educational platform where community members can work together on creating, validating, and promoting content that supports the Zcash & Privacy technology ecosystems.";
 
   return {
     metadataBase: new URL("https://zechub.wiki"),
@@ -24,22 +30,22 @@ export const genMetadata = ({ title, description, image, url }: MetadataOpts) =>
       images: image || defaultImage,
       siteName: "ZecHub Wiki",
       type: "website",
-      url: url || defaultUrl
+      url: url || defaultUrl,
     },
     twitter: {
       title: title || defaultTitle,
       card: "summary_large_image",
       description: description || defaultDescription,
       image: image || defaultImage,
-      url: url || defaultUrl
-    }
+      url: url || defaultUrl,
+    },
   };
 };
 
 export const getName = (item: string) => {
-  const newItem = transformUri(item.substring(item.lastIndexOf('/') + 1), true);
-  const newFolder = newItem.split('_').join(' ');
-  if (newFolder === 'Glossary And FAQs') {
+  const newItem = transformUri(item.substring(item.lastIndexOf("/") + 1), true);
+  const newFolder = newItem.split("_").join(" ");
+  if (newFolder === "Glossary And FAQs") {
     return `Glossary & FAQ's`;
   } else {
     return newFolder;
@@ -47,11 +53,13 @@ export const getName = (item: string) => {
 };
 
 export const getDynamicRoute = (slug: string): string => {
-  let uri = '';
+  let uri = "";
   for (let i = 0; i < slug.length; i++) {
-    uri += '/' + slug[i];
+    uri += "/" + slug[i];
   }
-  return  uri === '/contribute/community-infrastructure' ? `/site/contribute/Community_Infrastructure.md` : `/site${transformUri(uri)}.md`;
+  return uri === "/contribute/community-infrastructure"
+    ? `/site/contribute/Community_Infrastructure.md`
+    : `/site${transformUri(uri)}.md`;
 };
 
 export const getFiles = (data: any) => {
@@ -59,11 +67,11 @@ export const getFiles = (data: any) => {
 };
 
 export const getFolders = (folder: string[]) => {
-  return folder.filter((st: string) => !st.endsWith('.md'));
+  return folder.filter((st: string) => !st.endsWith(".md"));
 };
 
 export const firstFileForFolders = async (folders: string[]) => {
-  let files: string[] = [''];
+  let files: string[] = [""];
   for (let i = 0; i <= folders.length; i++) {
     const res = await getRootCached(folders[i]);
     files.push(res[0]);
@@ -73,69 +81,73 @@ export const firstFileForFolders = async (folders: string[]) => {
 
 export const getBanner = (name: string) => {
   const transformedName = transformUri(name);
-  const banner = contentBanners.find(banner => banner.name === transformedName);
-  return banner ? banner.url : '';
+  const banner = contentBanners.find(
+    (banner) => banner.name === transformedName,
+  );
+  return banner ? banner.url : "";
 };
 
 const uppercaseWords = [
-  'Zec',
-  'Dex',
-  'Nft',
-  'Zcap',
-  'Zfav',
-  'Snarks',
-  'Frost',
-  '2fa',
-  'Pgp',
-  'I2p',
-  'Dao',
+  "Zec",
+  "Dex",
+  "Nft",
+  "Zcap",
+  "Zfav",
+  "Snarks",
+  "Frost",
+  "2fa",
+  "Pgp",
+  "I2p",
+  "Dao",
 ];
 const lowercaseWords = [
-  'Guides',
-  'Tutorials',
-  'Contribute',
-  'In',
-  'The',
-  'Vs',
-  'And',
-  'Is',
-  'Zk',
+  "Guides",
+  "Tutorials",
+  "Contribute",
+  "In",
+  "The",
+  "Vs",
+  "And",
+  "Is",
+  "Zk",
 ];
 const specialWordsMap = {
-  Non_Custodial: 'Non-Custodial',
-  Zero_Knowledge: 'Zero-Knowledge',
-  Defi: 'DeFi',
-  'Glossary And FAQs': `Glossary & FAQ's`,
-  Z2z: 'z2z',
-  Faq: 'FAQ',
-  ZECHub: 'ZecHub',
-  ZEChub: 'ZecHub',
-  Av_Club: 'AV_Club',
-  guides_For_Creators: 'Guides_for_Creators',
-  Grapheneos: 'GrapheneOS',
-  Vpn: 'VPN',
-  Dvpn: 'DVPN',
-  zk_Shielded: 'ZK_Shielded',
-  ZECweekly: 'ZecWeekly',
-  ZECWeekly: 'ZecWeekly',
-  Help_Build_ZecHub: 'Help_build_ZecHub',
-  zkool_Multisig : 'Zkool_Multisig',
-  Zenith_installation : 'Zenith_Installation',
-  Raspberry_Pi5_Zebra_Lightwalletd_Zingo : "Raspberry_pi5_Zebra_Lightwalletd_Zingo",
-  Btcpayserver_Zcash_Plugin : "BTCPayServer_Zcash_Plugin",
-  Uris: 'URIs',
-  Free2z_Livestreaming : "Free2Z_Livestreaming",
-  Avalanche_Redbridge : "Avalanche_RedBridge",
-  Raspberry_Pi_4_Full_Node : "Raspberry_Pi_4_Full_Node",
-  Raspberry_Pi_4_Zebra_Node : "Raspberry_pi_4_Zebra_Node",
-  Coinholder_Directed_Retroactive_Grants : "CoinHolder_Directed_Retroactive_Grants",
-  zkav : 'ZKAV',
+  Non_Custodial: "Non-Custodial",
+  Zero_Knowledge: "Zero-Knowledge",
+  Defi: "DeFi",
+  "Glossary And FAQs": `Glossary & FAQ's`,
+  Z2z: "z2z",
+  Faq: "FAQ",
+  ZECHub: "ZecHub",
+  ZEChub: "ZecHub",
+  Av_Club: "AV_Club",
+  guides_For_Creators: "Guides_for_Creators",
+  Grapheneos: "GrapheneOS",
+  Vpn: "VPN",
+  Dvpn: "DVPN",
+  zk_Shielded: "ZK_Shielded",
+  ZECweekly: "ZecWeekly",
+  ZECWeekly: "ZecWeekly",
+  Help_Build_ZecHub: "Help_build_ZecHub",
+  zkool_Multisig: "Zkool_Multisig",
+  Zenith_installation: "Zenith_Installation",
+  Raspberry_Pi5_Zebra_Lightwalletd_Zingo:
+    "Raspberry_pi5_Zebra_Lightwalletd_Zingo",
+  Btcpayserver_Zcash_Plugin: "BTCPayServer_Zcash_Plugin",
+  Uris: "URIs",
+  Free2z_Livestreaming: "Free2Z_Livestreaming",
+  Avalanche_Redbridge: "Avalanche_RedBridge",
+  Raspberry_Pi_4_Full_Node: "Raspberry_Pi_4_Full_Node",
+  Raspberry_Pi_4_Zebra_Node: "Raspberry_pi_4_Zebra_Node",
+  Coinholder_Directed_Retroactive_Grants:
+    "CoinHolder_Directed_Retroactive_Grants",
+  zkav: "ZKAV",
 };
 
 export const transformUri = (uri: string, ignoreLowerCase = false) => {
   let transformed = uri
-    .replace(/\b\w/g, (l) => l.toUpperCase())  
-    .replace(/-/g, '_');
+    .replace(/\b\w/g, (l) => l.toUpperCase())
+    .replace(/-/g, "_");
 
   if (!ignoreLowerCase)
     lowercaseWords.forEach((word) => {
@@ -155,9 +167,9 @@ export const transformUri = (uri: string, ignoreLowerCase = false) => {
 
 export const transformGithubFilePathToWikiLink = (path: string) => {
   return path
-    .replace('site/', '')
+    .replace("site/", "")
     .replace(/\w/g, (l) => l.toLowerCase())
-    .replace(/_/g, '-');
+    .replace(/_/g, "-");
 };
 
 export const formatString = {
@@ -165,7 +177,7 @@ export const formatString = {
     return txt[0].toUpperCase() + txt.slice(1);
   },
   removeUnderscore: (str: string) => {
-    return str.split('_').join(' ');
+    return str.split("_").join(" ");
   },
   /**
    * The function wrap a sentence at particular length of characters
@@ -176,7 +188,7 @@ export const formatString = {
   wordWrap: (txt: string, wrapAfter: number) => {
     txt = txt.trim();
     if (txt.length > wrapAfter) {
-      return txt.slice(0, wrapAfter) + '...';
+      return txt.slice(0, wrapAfter) + "...";
     }
 
     return txt;
@@ -188,3 +200,9 @@ export const formatNumber = (n: number) =>
   new Intl.NumberFormat("en-US", {
     maximumFractionDigits: 2,
   }).format(n);
+
+export const formatCurrencyToUSD = (value: number) =>
+  new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
