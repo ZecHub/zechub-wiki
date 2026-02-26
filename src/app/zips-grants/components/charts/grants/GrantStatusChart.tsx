f3dc4a8b241f32ccfdc78f89c5ffd8dabfc7a071
amochuko <7204868+amochuko@users.noreply.github.com>
2026-02-26 19:32:19 +0100

    refactor GrantStatusChart

--- src/app/zips-grants/components/charts/grants/GrantStatusChart.tsx
+++ src/app/zips-grants/components/charts/grants/GrantStatusChart.tsx
@@ -1,13 +1,57 @@
-import { Pie, Legend, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+import { CHART_COLORS } from "@/app/zips-grants/lib/chart-colors";
+import { formatCurrencyToUSD } from "@/lib/helpers";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
 
 type Props = {
-  data: { name: string; value: string }[];
+  data: { name: string; value: number }[];
 };
 
+const RADIAN = Math.PI / 180;
+
+function renderPercentageLabel({
+  cx,
+  cy,
+  midAnggle,
+  innerRadius,
+  outerRadius,
+  percent,
+}: any) {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAnggle * RADIAN);
+  const y = cy + radius * Math.sin(-midAnggle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor="middle"
+      dominantBaseline={"center"}
+      className="text-sm font-semibold"
+    >
+      {(percent * 100).toFixed(0)}%
+    </text>
+  );
+}
+
 export function GrantStatusChart(props: Props) {
+
+
   return (
     <div className="bg-white dark:bg-slate-900 p-6 rounded-xl shadow">
-      <h3 className="text-lg font-semibold mb-4">Grant Status Distribution</h3>
+      <div>
+        <h3 className="text-lg font-semibold mb-4">
+          Grant Status Distribution
+        </h3>
+        <p className="text-sm text-slate-500 mb-4">Active va close grants</p>
+      </div>
 
       <ResponsiveContainer width="100%" height={300}>
         <PieChart>
@@ -15,9 +59,17 @@ export function GrantStatusChart(props: Props) {
             data={props.data}
             dataKey={"value"}
             nameKey={"name"}
-            outerRadius={100}
-          />
-          <Tooltip />
+            outerRadius={110}
+            innerRadius={70}
+            paddingAngle={3}
+            label={renderPercentageLabel}
+            isAnimationActive
+          >
+            {props.data.map((e, i) => {
+              return <Cell key={i} fill={CHART_COLORS.financial[e.name]} />;
+            })}
+          </Pie>
+          <Tooltip formatter={(val: number) => `${formatCurrencyToUSD(val)}`} />
           <Legend />
         </PieChart>
       </ResponsiveContainer>
