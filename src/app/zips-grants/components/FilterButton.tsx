56fdc9b277bdb6772b55327895ba19fcaacae4ce
amochuko <7204868+amochuko@users.noreply.github.com>
2026-02-27 18:55:10 +0100

    refactor css

--- src/app/zips-grants/components/FilterButton.tsx
+++ src/app/zips-grants/components/FilterButton.tsx
@@ -9,10 +9,10 @@ export function FilterButton(props: Props) {
     <button
       key={props.search}
       onClick={() => props.onClick(props.search)}
-      className={`px-3 py-1.5 text-xs font-medium rounded-full border transition-colors ${
+      className={`px-3 py-1.5 text-xs font-medium bg-amber-400 rounded-full border dark:border-slate-800 border-slate-100/50 transition-colors hover:cursor-pointer ${
         props.filter === props.search
-          ? "bg-primary text-primary-foreground border-primary"
-          : "dark:bg-slate-800 bg-gray-500 text-slate-100 dark:text-muted-foreground border-border hover:border-primary/50 hover:text-foreground"
+          ? "bg-primary text-primary-foreground dark:border-primary"
+          : "bg-inherit"
       }`}
     >
       {props.search}
