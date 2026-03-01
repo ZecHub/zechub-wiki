1993c3b4f6f487b89f38f770972e7be7c327dce4
dismad <81990132+dismad@users.noreply.github.com>
2026-02-28 22:34:38 -0800

    missing script

--- public/data/zcash/zcashShieldedMetrics/ShieldedCurrents/extractTransferTXs.sh
+++ public/data/zcash/zcashShieldedMetrics/ShieldedCurrents/extractTransferTXs.sh
@@ -3,15 +3,14 @@
 startBlock="${1}"   #1 represent 1st argument
 endBlock="${2}"     #2 represent 2st argument
 
-#./listTXs.sh $startBlock $endBlock > mytxids
 
-t=$(cat mytxids | xargs -n1 ./txDetails.sh | jq -r '.vout | length' | paste -sd+ | bc)
-o=$(cat mytxids | xargs -n1 ./txDetails.sh | jq .orchard | jq -r '.actions | length' | paste -sd+ | bc)
-s=$(cat mytxids | xargs -n1 ./txDetails.sh | jq -r ' .vShieldedOutput | length ' | paste -sd+ | bc)
+
+#time ./listTXs.sh $startBlock $endBlock | xargs -n1 ./txDetails.sh | jq .vout.[].n | wc -l 
+
+t=$(./listTXs.sh $startBlock $endBlock | xargs -n1 ./txDetails.sh | jq -r '.vout | length' | paste -sd+ | bc)
+o=$(./listTXs.sh $startBlock $endBlock | xargs -n1 ./txDetails.sh | jq .orchard | jq -r '.actions | length' | paste -sd+ | bc)
+s=$(./listTXs.sh $startBlock $endBlock | xargs -n1 ./txDetails.sh | jq -r ' .vShieldedOutput | length ' | paste -sd+ | bc)
 
 result=$(echo "$t + $o + $s" | bc)
 
-echo "$t t transfer txs"
-echo "$s s transfer txs"
-echo "$o o transfer txs"
-echo "$result"
+echo "$result transfer transactions"
