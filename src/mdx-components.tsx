a2fef712197d674be9c62debcb279f17d8f4e618
dismad <81990132+dismad@users.noreply.github.com>
2026-03-08 17:26:10 -0700

    Remove ZecToZatsConverter from MDX components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,9 +1,7 @@
 import type { MDXComponents } from 'mdx/types'
-import ZecToZatsConverter from '@/components/Converter/ZecToZatsConverter'
- 
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    ZecToZatsConverter,
   }
 }
