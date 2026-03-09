a25aa6c2c4fbccb8ebdf31b64a03bece2196b0d7
dismad <81990132+dismad@users.noreply.github.com>
2026-03-08 21:31:15 -0700

    Update mdx-components.tsx

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -4,6 +4,6 @@ import ZecToZatsConverter from '@/components/Converter/ZecToZatsConverter'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    ZecToZatsConverter,   //  This line makes <ZecToZatsConverter /> work in Markdown!
+    ZecToZatsConverter,
   }
 }
