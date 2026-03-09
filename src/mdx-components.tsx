924ff30a9f6e241a56905c41813f33547e05be0c
dismad <81990132+dismad@users.noreply.github.com>
2026-03-08 20:35:14 -0700

    Add ZecToZatsConverter to MDX components
    Added ZecToZatsConverter to MDX components for use in Markdown.

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,9 @@
 import type { MDXComponents } from 'mdx/types'
+import ZecToZatsConverter from '@/components/Converter/ZecToZatsConverter'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    ZecToZatsConverter,   //  This line makes <ZecToZatsConverter /> work in Markdown!
   }
 }
