34a05f01cf99571936392a1ea6224b30a4de8d6b
teslasdev <teslasdeveloper@gmail.com>
2026-03-12 01:04:53 +0100

    push eod bug fixes

--- src/app/developers/page.tsx
+++ src/app/developers/page.tsx
@@ -3,8 +3,16 @@ import { genMetadata } from '@/lib/helpers';
 import DeveloperPage from './DeveloperPage';
 import { getDictionary } from '@/lib/getDictionary';
 
+type DevelopersDictionary = {
+  pages?: {
+    developers?: {
+      title?: string;
+    };
+  };
+};
+
 export async function generateMetadata(): Promise<Metadata> {
-  const dict = await getDictionary();
+  const dict = (await getDictionary()) as DevelopersDictionary;
   return genMetadata({
     title: dict.pages?.developers?.title || "Zcash Developer Resources",
     url: "https://zechub.wiki/using-zcash/blockchain-explorers",
