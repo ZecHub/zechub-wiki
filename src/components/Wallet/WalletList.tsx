023a10255098b9c4866129a240d99192977697a6
dismad <81990132+dismad@users.noreply.github.com>
2026-03-16 16:03:01 -0700

    Update WalletList.tsx

--- src/components/Wallet/WalletList.tsx
+++ src/components/Wallet/WalletList.tsx
@@ -213,7 +213,7 @@ const WalletList: React.FC<Props> = ({ allWallets }) => {
         </div>
       </div>
       <section className="h-auto w-full md:w-[80%]">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 md:gap-8">
           {sortedWallets.map((wallet) => (
             <WalletItem
               key={wallet.title}
