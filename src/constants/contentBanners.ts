6bb6845f02e30e69731563a4238bdbbed209c3e3
Jhei001 <olajoy16@gmail.com>
2026-03-17 22:31:15 +0100

    contentBanners.ts update

--- src/constants/contentBanners.ts
+++ src/constants/contentBanners.ts
@@ -1,51 +1,82 @@
 export const contentBanners = [
-    {
-        name: "Start_Here", 
-        url: '/content-banners/bannerstarthere.png'
-    },
-    {
-        name: "tutorials",
-        url: '/wiki-banner.avif'
-    },
-    {
-        name: "Using_Zcash",
-        url: '/content-banners/usingzcash.png'
-    },
-    {
-        name: "guides",
-        url: '/content-banners/bannerguides.png'
-    },
-    {
-        name: "Zcash_Tech",
-        url: '/content-banners/bannertech.png'
-    },
-    {
-        name: "Zcash_Organizations",
-        url: '/content-banners/bannerorgs.png'
-    },
-    {
-        name: "Zcash_Community",
-        url: '/content-banners/bannerecosystem2.png'
-    },
-    {
-        name: "ZFAV_Club", 
-        url: '/content-banners/ZFAVbanner.png'
-    },
-    {
-        name: "Privacy_Tools", 
-        url: '/content-banners/bannerPrivacyTools.png'
-    },
-    {
-        name: "Research", 
-        url: '/wiki-banner.avif'
-    },
-    {
-        name: "Glossary_and_FAQs", 
-        url: '/content-banners/bannerglossary.png'
-    },
-    {
-        name: "contribute", 
-        url: '/wiki-banner.avif'
-    }
-
-]
+  {
+    name: "Start_Here",
+    url: "/content-banners/bannerstarthere.jpg",
+  },
+  {
+    name: "Start_Here_Dark",
+    url: "/content-banners/bannerstarthere_Dark.jpg",
+  },
+  {
+    name: "tutorials",
+    url: "/wiki-banner.avif",
+  },
+  {
+    name: "Using_Zcash",
+    url: "/content-banners/usingzcash.png",
+  },
+  {
+    name: "guides",
+    url: "/content-banners/bannerguides.jpg",
+  },
+  {
+    name: "guides_Dark",
+    url: "/content-banners/bannerguides_Dark.jpg",
+  },
+  {
+    name: "Zcash_Tech",
+    url: "/content-banners/bannertech.jpg",
+  },
+  {
+    name: "Zcash_Tech_Dark",
+    url: "/content-banners/bannertech_Dark.jpg",
+  },
+  {
+    name: "Zcash_Organizations",
+    url: "/content-banners/bannerorgs.jpg",
+  },
+  {
+    name: "Zcash_Organizations_Dark",
+    url: "/content-banners/bannerorgs_Dark.jpg",
+  },
+  {
+    name: "Zcash_Community",
+    url: "/content-banners/bannerecosystem2.png",
+  },
+  {
+    name: "ZFAV_Club",
+    url: "/content-banners/ZFAVbanner.png",
+  },
+  {
+    name: "Privacy_Tools",
+    url: "/content-banners/bannerPrivacyTools.jpg",
+  },
+  {
+    name: "Privacy_Tools_Dark",
+    url: "/content-banners/bannerPrivacyTools_Dark.jpg",
+  },
+  {
+    name: "Research",
+    url: "/content-banners/bannerResearch.jpg",
+  },
+  {
+    name: "Research_Dark",
+    url: "/content-banners/bannerResearch_Dark.jpg",
+  },
+  {
+    name: "Glossary_and_FAQs",
+    url: "/content-banners/bannerglossary.jpg",
+  },
+  {
+    name: "Glossary_and_FAQs_Dark",
+    url: "/content-banners/bannerglossary_Dark.jpg",
+  },
+  {
+    name: "contribute",
+    url: "/content-banners/contribute.jpg",
+  },
+  {
+    name: "contribute_Dark",
+    url: "/content-banners/contribute_Dark.jpg",
+  },
+];
