85e28a207a22bc6bd47bf22a47e09cf733fa6c23
amochuko <7204868+amochuko@users.noreply.github.com>
2026-04-25 14:06:08 +0100

    add new key

--- src/app/tools/config.ts
+++ src/app/tools/config.ts
@@ -1,8 +1,10 @@
 export const config = {
+  WIDGET_CONTAINER: "#payment-request-container",
   env: {
-    NEXT_PUBLIC_WIDGET_API_BASE_URL:
-      String(process.env.NEXT_PUBLIC_WIDGET_API_BASE_URL),
+    NEXT_PUBLIC_WIDGET_API_BASE_URL: String(
+      process.env.NEXT_PUBLIC_WIDGET_API_BASE_URL,
+    ),
     NEXT_PUBLIC_API_BASE_URL_EMBED_CODE:
-      process.env.NEXT_PUBLIC_API_BASE_URL_EMBED_CODE,
+      String(process.env.NEXT_PUBLIC_API_BASE_URL_EMBED_CODE),
   },
 };
