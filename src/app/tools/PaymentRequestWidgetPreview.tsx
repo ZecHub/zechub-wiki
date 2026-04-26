3aeb4138ddb25b5fa37f1a1013cca07f1f386542
amochuko <7204868+amochuko@users.noreply.github.com>
2026-04-26 03:16:15 +0100

    include PaymentRequestWidgetCodeSnippet comp

--- src/app/tools/PaymentRequestWidgetPreview.tsx
+++ src/app/tools/PaymentRequestWidgetPreview.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { memo, useEffect, useRef, useState } from "react";
 import { config } from "./config";
+import PaymentRequestWidgetCodeSnippet, {
+  Modal,
+} from "./PaymentRequestWidgetCodeSnippet";
 
 interface Props {
   config: {
@@ -16,12 +19,13 @@ interface Props {
   };
 }
 
-export function PaymentRequestWidgetPreview(props: Props) {
+function PaymentRequestWidgetPreview(props: Props) {
   const { config: cfg } = props;
 
   const containerRef = useRef<HTMLDivElement>(null);
   const scriptRef = useRef<HTMLScriptElement | null>(null);
   const target = cfg.target.replace("#", "");
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -55,9 +59,28 @@ export function PaymentRequestWidgetPreview(props: Props) {
   }, [cfg]);
 
   return (
-    <>
+    <div className="flex gap-6">
       {/* required container for the embed script */}
       <div id={target} ref={containerRef}></div>
-    </>
+      
+      <button
+        disabled={cfg.disabled}
+        className={`px-6 text-black rounded-xl font-semibold text-widget-dark-bg bg-linear-to-r from-[#F4B728] to-[#d9a520] shadow-lg active:scale-[0.98] transition-all duration-200 disabled:opacity-60 disabled:cursor-not-allowed disabled:transform-none disabled:shadow-none cursor-pointer`}
+        onClick={() => setOpen(true)}
+      >
+        Preview Code Snippet
+      </button>
+
+      <Modal isOpen={open} onClose={() => setOpen(false)}>
+        <PaymentRequestWidgetCodeSnippet
+          config={{
+            ...cfg,
+            label: cfg.label!,
+          }}
+        />
+      </Modal>
+    </div>
   );
 }
+
+export default memo(PaymentRequestWidgetPreview);
