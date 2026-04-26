5a164a26eeab5e9aed234d2681f70ac5f6a13b36
amochuko <7204868+amochuko@users.noreply.github.com>
2026-04-26 01:15:20 +0100

    fix qrcode parser

--- src/app/api/payment-request-uri/qrcode/route.ts
+++ src/app/api/payment-request-uri/qrcode/route.ts
@@ -5,29 +5,37 @@ import { qrCodeBodySchema } from "./schema/qrcode.schema";
 
 export async function GET(req: NextRequest) {
   const data = req.nextUrl.searchParams.get("data");
+  const size = req.nextUrl.searchParams.get("size");
+  const dim = size?.split("x")[0];
 
   if (!data || typeof data != "string") {
     return NextResponse.json({ error: "Missing data" }, { status: 400 });
   }
 
   try {
-    const qrCode = await QRCode.toDataURL(data, { margin: 1, scale: 6 });
+    const qrCode = await QRCode.toDataURL(data, {
+      margin: 1,
+      scale: 10,
+      width: dim ? parseInt(dim) : 240,
+
+    });
     const base64 = qrCode.split(",")[1];
     const buffer = Buffer.from(base64, "base64");
 
-    return NextResponse.json(
-      { data: buffer },
-      {
-        headers: {
-          "Content-Type": "image/png",
-        },
+    return new NextResponse(new Uint8Array(buffer), {
+      headers: {
+        "Content-Type": "image/png",
+        "Content-Length": buffer.length.toString(),
+        "Cache-Control": "public, max-age=31536000, immutable",
       },
-    );
+    });
   } catch (err) {
-    const msg =
-      err instanceof Error ? err.message : "Failed to generate QRCode";
-
-    return NextResponse.json({ error: msg }, { status: 500 });
+    console.error(err);
+    
+    return NextResponse.json(
+      { error: '"Failed to generate QR code"' },
+      { status: 500 },
+    );
   }
 }
 
@@ -61,7 +69,8 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json({ data: { uri, qrData } }, { status: 200 });
   } catch (err) {
-    const msg = err instanceof Error ? err.message : "Failed process payment uri.";
+    const msg =
+      err instanceof Error ? err.message : "Failed process payment uri.";
 
     return NextResponse.json({ error: JSON.parse(msg) }, { status: 500 });
   }
