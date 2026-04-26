0110a856c2b0812278395179095bc5d23ab7fac8
amochuko <7204868+amochuko@users.noreply.github.com>
2026-04-26 14:34:13 +0100

    clean up

--- src/app/zcash-payment-uri/page.tsx
+++ src/app/zcash-payment-uri/page.tsx
@@ -2,8 +2,14 @@ import { ZcashPaymentURINextJs } from "../tools/zcash-payment-widget/adapters/ne
 
 export default function ZcashPaymentClient() {
   return (
-    <>
-      <h1>Payment URI</h1>
+    <div className="flex flex-col justify-center items-center mx-auto mt-12 gap-12">
+      <div className="text-center space-y-4">
+        <h1 className="text-3xl font-medium">Zcash Payment URI Widget </h1>
+        <p className="text-[18px] font-md">
+          This button component demostrates the usage of the Zcash payment uri
+          sdk for Next.js Adapter demo
+        </p>
+      </div>
       <ZcashPaymentURINextJs
         apiBase="https://zechub.wiki/api"
         address="zs1r3pp4354ewt5g970uc5r6gu4g8p0egmwwrrd6a0dsduvx92jxj0j9zcjjrkyx9wphf5ggux9ssg"
@@ -12,6 +18,6 @@ export default function ZcashPaymentClient() {
         theme="dark"
         disabled={false}
       />
-    </>
+    </div>
   );
 }
