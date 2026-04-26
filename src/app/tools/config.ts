ff32532a952f241b7aab1369d29a2e68e3493551
amochuko <7204868+amochuko@users.noreply.github.com>
2026-04-26 10:38:02 +0100

    add allow_origin

--- src/app/tools/config.ts
+++ src/app/tools/config.ts
@@ -1,10 +1,14 @@
 export const config = {
   WIDGET_CONTAINER: "#payment-request-container",
   env: {
+    ACCESS_CONTROL_ALLOW_ORIGIN: String(
+      process.env.ACCESS_CONTROL_ALLOW_ORIGIN,
+    ),
     NEXT_PUBLIC_WIDGET_API_BASE_URL: String(
       process.env.NEXT_PUBLIC_WIDGET_API_BASE_URL,
     ),
-    NEXT_PUBLIC_API_BASE_URL_EMBED_CODE:
-      String(process.env.NEXT_PUBLIC_API_BASE_URL_EMBED_CODE),
+    NEXT_PUBLIC_API_BASE_URL_EMBED_CODE: String(
+      process.env.NEXT_PUBLIC_API_BASE_URL_EMBED_CODE,
+    ),
   },
 };
