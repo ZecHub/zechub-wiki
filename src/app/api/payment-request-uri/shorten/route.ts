943f6f7310fbec4fcc959bd1396b889fca060a71
amochuko <7204868+amochuko@users.noreply.github.com>
2026-04-26 10:37:30 +0100

    resolve cors headers

--- src/app/api/payment-request-uri/shorten/route.ts
+++ src/app/api/payment-request-uri/shorten/route.ts
@@ -1,8 +1,22 @@
+import { config } from "@/app/tools/config";
 import { nanoid } from "nanoid";
 import { NextRequest, NextResponse } from "next/server";
 
 // TODO: Using memory store (can be changed to db)
 export const urlStore: Record<string, string> = {}; // shortId => full URI
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": `${config.env.ACCESS_CONTROL_ALLOW_ORIGIN}`,
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "ACCESS-CONTROL-ALLOW-HEADERS": "CONTENT-TYPE",
+};
+
+export async function OPTIONS(req: NextRequest) {
+  return new NextResponse(null, {
+    status: 204,
+    headers: corsHeaders,
+  });
+}
 
 export async function POST(req: NextRequest) {
   try {
@@ -16,10 +30,19 @@ export async function POST(req: NextRequest) {
     const shortId = nanoid(8);
     urlStore[shortId] = uri;
 
-    const url = req.url.split("/api")[0];
+    const url = config.env.NEXT_PUBLIC_WIDGET_API_BASE_URL;
     const shortUrl = `${url}/api/payment-request-uri/shorten/${shortId}`;
-    
-    return NextResponse.json({ shortUrl }, { status: 200 });
+
+    return NextResponse.json(
+      { shortUrl },
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+          ...corsHeaders,
+        },
+      },
+    );
   } catch (err) {
     const msg =
       err instanceof Error ? err.message : "Failed process payment uri.";
