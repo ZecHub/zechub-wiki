0af38cfa8a37126a04e82992bef97232df11ed84
amochuko <7204868+amochuko@users.noreply.github.com>
2026-05-08 09:34:19 +0100

    adjust css

--- src/components/visualizer/distributed-databases/slides/Failed.tsx
+++ src/components/visualizer/distributed-databases/slides/Failed.tsx
@@ -9,7 +9,7 @@ export default function FailedComp(props: FailedProps) {
     <motion.div
       initial={{ opacity: 0, y: 8 }}
       animate={{ opacity: 1, y: 0 }}
-      className={`absolute bottom-4 left-1/2 -translate-x-1/2 rounded-full border px-4 py-1.5 text-xs font-semibold uppercase tracking-widest text-center md:-bottom-2.5 ${props.css}`}
+      className={`absolute bottom-4 min-w-68 left-1/2 -translate-x-1/2 rounded-full border px-4 py-1.5 text-xs font-semibold uppercase tracking-widest text-center md:-bottom-2.5 ${props.css}`}
     >
       {props.label}
     </motion.div>
