07e8705a12aed74c50f7e436d2a59f1770568106
onajifortune <onajifortunejnr@gmail.com>
2026-05-13 20:35:06 +0100

    Test

---
+++ src/app/test/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import { genMetadata } from "@/lib/helpers";
+import QuickStartPage from "./quick-start/page";
+import { getDictionary } from "@/lib/getDictionary";
+
+type DevelopersDictionary = {
+  pages?: {
+    developers?: {
+      title?: string;
+    };
+  };
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+  const dict = (await getDictionary()) as DevelopersDictionary;
+  return genMetadata({
+    title: dict.pages?.developers?.title || "Zcash Developer Resources",
+    url: "https://zechub.wiki/using-zcash/blockchain-explorers",
+  }) as Metadata;
+}
+
+export default function Page() {
+  return <QuickStartPage />;
+}
