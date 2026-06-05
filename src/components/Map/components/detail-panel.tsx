c9700c3bf318c699515c9bac28101fbf2ead2651
amochuko <7204868+amochuko@users.noreply.github.com>
2026-06-05 18:39:27 +0100

    impl DetailPanel comp

---
+++ src/components/Map/components/detail-panel.tsx
@@ -0,0 +1,79 @@
+import { getBrandColor } from "../helpers";
+import { StoreEntry } from "../SpednMap";
+
+interface DetailPanelProps {
+  store: StoreEntry | null;
+  onClose: () => void;
+}
+export function DetailPanel(props: DetailPanelProps) {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        bottom: 14,
+        right: 14,
+        width: 250,
+        background: "var(--color-background-primary)",
+        border: "0.5px solid var(--color-border-secondary)",
+        borderRadius: "var(--border-radius-lg)",
+        padding: "14px 14px 16px",
+        zIndex: 900,
+        boxShadow: "0 4px 20px rgba(0,0,0,0.12)",
+      }}
+    >
+      {/* close */}
+      <button
+        style={{
+          position: "absolute",
+          top: 10,
+          right: 10,
+          background: "none",
+          border: "none",
+          cursor: "pointer",
+          color: "var(--color-text-secondary)",
+          fontSize: 14,
+          padding: 2,
+        }}
+      >
+        x
+      </button>
+
+      {/* Brand name */}
+      <div
+        style={{
+          fontSize: 13,
+          fontWeight: 500,
+          color: "var(--color-text-primary)",
+          marginBottom: 5,
+          paddingRight: 20,
+        }}
+      >
+        <span
+          style={{
+            display: "inline-block",
+            width: 8,
+            height: 8,
+            borderRadius: "50%",
+            background: getBrandColor(props.store?.brand!),
+            marginRight: 7,
+            flexShrink: 0,
+          }}
+        />
+        {props.store?.brand}
+      </div>
+
+      {/* Address */}
+      <p
+        style={{
+          fontSize: 12,
+          color: "var(--color-text-secondary)",
+          lineHeight: 1.55,
+          marginBottom: 10,
+          paddingLeft: 15,
+        }}
+      >
+        {props.store?.address}
+      </p>
+    </div>
+  );
+}
